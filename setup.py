e13f57143d7cf116a5f174e89ae5b44f61724b3d
Fernando Nogueira <fmfnogueira@gmail.com>
2015-06-10 11:32:36 -0400

    Changes the email field to one single string, and small style changes.

--- setup.py
+++ setup.py
@@ -2,19 +2,21 @@ from setuptools import setup, find_packages
 
 install_requires = [
     'numpy',
+    'scipy',
     'scikit-learn',
     ]
 
 setup(name='UnbalancedDataset',
       version='0.1',
-      description='Python module with numerous re-sampling strategies to deal with classification'
-                  'of data-sets with strong between class imbalance.',
+      description='Python module with numerous re-sampling strategies to deal '
+                  'with classification of data-sets with strong between class '
+                  'imbalance.',
       classifiers=[
           "Programming Language :: Python :: 2.7",
           "Programming Language :: Python :: 3.4",
           ],
-      author='Fernando Nogueira & Guillaume Lemaitre',
-      author_email=['fmfnogueira@gmail.com', 'guillaume.lemaitre@udg.edu'],
+      author='Fernando Nogueira, Guillaume Lemaitre',
+      author_email='fmfnogueira@gmail.com, guillaume.lemaitre@udg.edu',
       url='https://github.com/fmfn/UnbalancedDataset',
       packages=find_packages(),
       include_package_data=True,
