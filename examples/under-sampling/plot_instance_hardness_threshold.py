85ef41cb384eef87f5c8dd5c4f1e5ca6c1669c95
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-06-18 15:07:18 +0200

    Correct the error with PCA in the example

--- examples/under-sampling/plot_instance_hardness_threshold.py
+++ examples/under-sampling/plot_instance_hardness_threshold.py
@@ -31,7 +31,7 @@ X, y = make_classification(n_classes=2, class_sep=1., weights=[0.05, 0.95],
                            n_samples=5000, random_state=10)
 
 pca = PCA(n_components=2)
-X = pca.fit_transform(X)
+X_vis = pca.fit_transform(X)
 
 # Two subplots, unpack the axes array immediately
 f, axs = plt.subplots(2, 2)
@@ -39,23 +39,23 @@ f, axs = plt.subplots(2, 2)
 axs = [a for ax in axs for a in ax]
 for ax, ratio in zip(axs, [0.0, 0.1, 0.3, 0.5]):
     if ratio == 0.0:
-        ax.scatter(X[y == 0, 0], X[y == 0, 1], label="Class #0", alpha=0.5,
-                   edgecolor=almost_black, facecolor=palette[0],
-                   linewidth=0.15)
-        ax.scatter(X[y == 1, 0], X[y == 1, 1], label="Class #1", alpha=0.5,
-                   edgecolor=almost_black, facecolor=palette[2],
+        ax.scatter(X_vis[y == 0, 0], X_vis[y == 0, 1], label="Class #0",
+                    alpha=0.5, edgecolor=almost_black, facecolor=palette[0],
+                    linewidth=0.15)
+        ax.scatter(X_vis[y == 1, 0], X_vis[y == 1, 1], label="Class #1",
+                   alpha=0.5, edgecolor=almost_black, facecolor=palette[2],
                    linewidth=0.15)
         ax.set_title('Original set')
     else:
         estimator = SVC(probability=True)
         iht = InstanceHardnessThreshold(estimator, ratio=ratio)
         X_res, y_res = iht.fit_transform(X, y)
-        print np.bincount(y_res)
+        X_res_vis = pca.transform(X_res)
 
-        ax.scatter(X_res[y_res == 0, 0], X_res[y_res == 0, 1],
+        ax.scatter(X_res_vis[y_res == 0, 0], X_res_vis[y_res == 0, 1],
                    label="Class #0", alpha=.5, edgecolor=almost_black,
                    facecolor=palette[0], linewidth=0.15)
-        ax.scatter(X_res[y_res == 1, 0], X_res[y_res == 1, 1],
+        ax.scatter(X_res_vis[y_res == 1, 0], X_res_vis[y_res == 1, 1],
                    label="Class #1", alpha=.5, edgecolor=almost_black,
                    facecolor=palette[2], linewidth=0.15)
         ax.set_title('Instance Hardness Threshold ({})'.format(ratio))
