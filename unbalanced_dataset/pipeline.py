72a16a76018551f2f6a443578bc7b703969775e0
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2016-06-26 17:20:45 +0200

    [WIP] Adding testing for pipeline (#1)
    Add testing for pipeline

--- unbalanced_dataset/pipeline.py
+++ unbalanced_dataset/pipeline.py
@@ -1,10 +1,10 @@
-﻿# Adapted from
-# https://github.com/scikit-learn/scikit-learn/blob/master/sklearn/pipeline.py
+﻿"""
+The :mod:`unbalanced_dataset.pipeline` module implements utilities to build
+ a composite estimator, as a chain of transforms, samples and estimators.
+"""
 
-"""
-The :mod:`unblanced_dataset.pipeline` module implements utilities to build a composite
-estimator, as a chain of transforms, samples and estimators.
-"""
+# Adapted from
+# https://github.com/scikit-learn/scikit-learn/blob/master/sklearn/pipeline.py
 
 # Author: Edouard Duchesnay
 #         Gael Varoquaux
@@ -13,6 +13,9 @@ estimator, as a chain of transforms, samples and estimators.
 #         Lars Buitinck
 #         chkoar
 # License: BSD
+
+from __future__ import print_function
+from __future__ import division
 
 from warnings import warn
 
@@ -29,7 +32,7 @@ class Pipeline(pipeline.Pipeline):
     """Pipeline of transforms and resamples with a final estimator.
 
     Sequentially apply a list of transforms, samples and a final estimator.
-    Intermediate steps of the pipeline must be transformers or resamplers, 
+    Intermediate steps of the pipeline must be transformers or resamplers,
     that is, they must implement fit, transform and sample methods.
     The final estimator only needs to implement fit.
 
@@ -38,14 +41,12 @@ class Pipeline(pipeline.Pipeline):
     For this, it enables setting parameters of the various steps using their
     names and the parameter name separated by a '__', as in the example below.
 
-    Read more in the :ref:`User Guide <pipeline>`.
-
     Parameters
     ----------
     steps : list
-        List of (name, transform) tuples (implementing fit/transform/fit_sample) that are
-        chained, in the order in which they are chained, with the last object
-        an estimator.
+        List of (name, transform) tuples (implementing
+        fit/transform/fit_sample) that are chained, in the order in which they
+        are chained, with the last object an estimator.
 
     Attributes
     ----------
@@ -53,35 +54,6 @@ class Pipeline(pipeline.Pipeline):
         Read-only attribute to access any step parameter by user given name.
         Keys are step names and values are steps parameters.
 
-    Examples
-    --------
-    >>> from sklearn import svm
-    >>> from sklearn.datasets import samples_generator
-    >>> from sklearn.feature_selection import SelectKBest
-    >>> from sklearn.feature_selection import f_regression
-    >>> from sklearn.pipeline import Pipeline
-    >>> # generate some data to play with
-    >>> X, y = samples_generator.make_classification(
-    ...     n_informative=5, n_redundant=0, random_state=42)
-    >>> # ANOVA SVM-C
-    >>> anova_filter = SelectKBest(f_regression, k=5)
-    >>> clf = svm.SVC(kernel='linear')
-    >>> anova_svm = Pipeline([('anova', anova_filter), ('svc', clf)])
-    >>> # You can set the parameters using the names issued
-    >>> # For instance, fit using a k of 10 in the SelectKBest
-    >>> # and a parameter 'C' of the svm
-    >>> anova_svm.set_params(anova__k=10, svc__C=.1).fit(X, y)
-    ...                                              # doctest: +ELLIPSIS
-    Pipeline(steps=[...])
-    >>> prediction = anova_svm.predict(X)
-    >>> anova_svm.score(X, y)                        # doctest: +ELLIPSIS
-    0.77...
-    >>> # getting the selected features chosen by anova_filter
-    >>> anova_svm.named_steps['anova'].get_support()
-    ... # doctest: +NORMALIZE_WHITESPACE
-    array([ True,  True,  True, False, False,  True, False,  True,  True, True,
-           False, False,  True, False,  True, False, False, False, False,
-           True], dtype=bool)
     """
 
     # BaseEstimator interface
@@ -99,8 +71,8 @@ class Pipeline(pipeline.Pipeline):
 
         for t in transforms:
             if (not (hasattr(t, "fit") or hasattr(t, "fit_transform") or
-                     hasattr(t, "fit_sample")) or not (hasattr(t, "transform")
-                                                       or hasattr(t, "sample"))):
+                     hasattr(t, "fit_sample")) or
+                not (hasattr(t, "transform") or hasattr(t, "sample"))):
                 raise TypeError("All intermediate steps of the chain should "
                                 "be transforms and implement fit and transform"
                                 " '%s' (type %s) doesn't)" % (t, type(t)))
@@ -130,7 +102,7 @@ class Pipeline(pipeline.Pipeline):
         return Xt, yt, fit_params_steps[self.steps[-1][0]]
 
     def fit(self, X, y=None, **fit_params):
-        """Fit all the transforms and samples one after the other and transform 
+        """Fit all the transforms and samples one after the other and transform
         the data, then fit the transformed data using the final estimator.
 
         Parameters
@@ -147,8 +119,8 @@ class Pipeline(pipeline.Pipeline):
         return self
 
     def fit_transform(self, X, y=None, **fit_params):
-        """Fit all the transforms and samples one after the other and 
-        transform or sample the data, then use fit_transform on 
+        """Fit all the transforms and samples one after the other and
+        transform or sample the data, then use fit_transform on
         transformed data using the final estimator.
 
         Parameters
@@ -169,8 +141,8 @@ class Pipeline(pipeline.Pipeline):
 
     @if_delegate_has_method(delegate='_final_estimator')
     def fit_sample(self, X, y=None, **fit_params):
-        """Fit all the transforms and samples one after the other and 
-        transform or sample the data, then use fit_sample on 
+        """Fit all the transforms and samples one after the other and
+        transform or sample the data, then use fit_sample on
         transformed data using the final estimator.
 
         Parameters
@@ -188,8 +160,8 @@ class Pipeline(pipeline.Pipeline):
 
     @if_delegate_has_method(delegate='_final_estimator')
     def sample(self, X, y):
-        """Applies transforms to the data, and the sample method of 
-        the final estimator. Valid only if the final estimator 
+        """Applies transforms to the data, and the sample method of
+        the final estimator. Valid only if the final estimator
         implements predict.
 
         Parameters
@@ -199,7 +171,7 @@ class Pipeline(pipeline.Pipeline):
             of the pipeline.
         """
         Xt = X
-        for name, transform in self.steps[:-1]:
+        for _, transform in self.steps[:-1]:
             if hasattr(transform, "fit_sample"):
                 pass
             else:
@@ -208,8 +180,8 @@ class Pipeline(pipeline.Pipeline):
 
     @if_delegate_has_method(delegate='_final_estimator')
     def predict(self, X):
-        """Applies transforms to the data, and the predict method of 
-        the final estimator. Valid only if the final estimator 
+        """Applies transforms to the data, and the predict method of
+        the final estimator. Valid only if the final estimator
         implements predict.
 
         Parameters
@@ -219,7 +191,7 @@ class Pipeline(pipeline.Pipeline):
             of the pipeline.
         """
         Xt = X
-        for name, transform in self.steps[:-1]:
+        for _, transform in self.steps[:-1]:
             if hasattr(transform, "fit_sample"):
                 pass
             else:
@@ -231,8 +203,8 @@ class Pipeline(pipeline.Pipeline):
         """Applies fit_predict of last step in pipeline after transforms
         and samples.
 
-        Applies fit_transforms or fit_samples of a pipeline to the data, 
-        followed by the fit_predict method of the final estimator in the 
+        Applies fit_transforms or fit_samples of a pipeline to the data,
+        followed by the fit_predict method of the final estimator in the
         pipeline. Valid only if the final estimator implements fit_predict.
 
         Parameters
@@ -260,7 +232,7 @@ class Pipeline(pipeline.Pipeline):
             of the pipeline.
         """
         Xt = X
-        for name, transform in self.steps[:-1]:
+        for _, transform in self.steps[:-1]:
             if hasattr(transform, "fit_sample"):
                 pass
             else:
@@ -280,7 +252,7 @@ class Pipeline(pipeline.Pipeline):
             of the pipeline.
         """
         Xt = X
-        for name, transform in self.steps[:-1]:
+        for _, transform in self.steps[:-1]:
             if hasattr(transform, "fit_sample"):
                 pass
             else:
@@ -300,7 +272,7 @@ class Pipeline(pipeline.Pipeline):
             of the pipeline.
         """
         Xt = X
-        for name, transform in self.steps[:-1]:
+        for _, transform in self.steps[:-1]:
             if hasattr(transform, "fit_sample"):
                 pass
             else:
@@ -320,7 +292,7 @@ class Pipeline(pipeline.Pipeline):
             of the pipeline.
         """
         Xt = X
-        for name, transform in self.steps:
+        for _, transform in self.steps:
             if hasattr(transform, "fit_sample"):
                 pass
             else:
@@ -345,8 +317,8 @@ class Pipeline(pipeline.Pipeline):
                  " pipeline.inverse_transform any more.", FutureWarning)
             X = X[None, :]
         Xt = X
-        for name, step in self.steps[::-1]:
-            if hasattr(transform, "fit_sample"):
+        for _, step in self.steps[::-1]:
+            if hasattr(step, "fit_sample"):
                 pass
             else:
                 Xt = step.inverse_transform(Xt)
@@ -369,13 +341,11 @@ class Pipeline(pipeline.Pipeline):
             steps of the pipeline.
         """
         Xt = X
-        for name, transform in self.steps[:-1]:
+        for _, transform in self.steps[:-1]:
             if hasattr(transform, "fit_sample"):
                 pass
             else:
-                print Xt.shape
                 Xt = transform.transform(Xt)
-                print Xt.shape
         return self.steps[-1][-1].score(Xt, y)
 
 
@@ -385,15 +355,6 @@ def make_pipeline(*steps):
     This is a shorthand for the Pipeline constructor; it does not require, and
     does not permit, naming the estimators. Instead, their names will be set
     to the lowercase of their types automatically.
-
-    Examples
-    --------
-    >>> from sklearn.naive_bayes import GaussianNB
-    >>> from sklearn.preprocessing import StandardScaler
-    >>> make_pipeline(StandardScaler(), GaussianNB(priors=None))    # doctest: +NORMALIZE_WHITESPACE
-    Pipeline(steps=[('standardscaler',
-                     StandardScaler(copy=True, with_mean=True, with_std=True)),
-                    ('gaussiannb', GaussianNB(priors=None))])
 
     Returns
     -------
