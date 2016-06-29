fa91ee10b6fba95a68a0e9abac3d662f38e2d8d8
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-06-29 19:13:22 +0200

    Finish the combine method

--- imblearn/combine/smote_enn.py
+++ imblearn/combine/smote_enn.py
@@ -22,8 +22,11 @@ class SMOTEENN(SamplerMixin):
         number of samples in the minority class over the the number of
         samples in the majority class.
 
-    random_state : int or None, optional (default=None)
-        Seed for random number generation.
+    random_state : int, RandomState instance or None, optional (default=None)
+        If int, random_state is the seed used by the random number generator;
+        If RandomState instance, random_state is the random number generator;
+        If None, the random number generator is the RandomState instance used
+        by np.random.
 
     verbose : bool, optional (default=True)
         Whether or not to print information about the processing.
@@ -60,15 +63,6 @@ class SMOTEENN(SamplerMixin):
 
     Attributes
     ----------
-    ratio : str or float
-        If 'auto', the ratio will be defined automatically to balance
-        the dataset. Otherwise, the ratio is defined as the
-        number of samples in the minority class over the the number of
-        samples in the majority class.
-
-    random_state : int or None
-        Seed for random number generation.
-
     min_c_ : str or int
         The identifier of the minority class.
 
@@ -100,75 +94,21 @@ class SMOTEENN(SamplerMixin):
                  k=5, m=10, out_step=0.5, kind_smote='regular',
                  size_ngh=3, kind_enn='all', n_jobs=-1, **kwargs):
 
-        """Initialise the SMOTE ENN object.
-
-        Parameters
-        ----------
-        ratio : str or float, optional (default='auto')
-            If 'auto', the ratio will be defined automatically to balance
-            the dataset. Otherwise, the ratio is defined as the
-            number of samples in the minority class over the the number of
-            samples in the majority class.
-
-        random_state : int or None, optional (default=None)
-            Seed for random number generation.
-
-        verbose : bool, optional (default=True)
-            Whether or not to print information about the processing.
-
-        k : int, optional (default=5)
-            Number of nearest neighbours to used to construct synthetic
-            samples.
-
-        m : int, optional (default=10)
-            Number of nearest neighbours to use to determine if a minority
-            sample is in danger.
-
-        out_step : float, optional (default=0.5)
-            Step size when extrapolating.
-
-        kind_smote : str, optional (default='regular')
-            The type of SMOTE algorithm to use one of the following
-            options: 'regular', 'borderline1', 'borderline2', 'svm'.
-
-        size_ngh : int, optional (default=3)
-            Size of the neighbourhood to consider to compute the average
-            distance to the minority point samples.
-
-        kind_sel : str, optional (default='all')
-            Strategy to use in order to exclude samples.
-
-            - If 'all', all neighbours will have to agree with the samples of
-            interest to not be excluded.
-            - If 'mode', the majority vote of the neighbours will be used in
-            order to exclude a sample.
-
-        n_jobs : int, optional (default=-1)
-            The number of threads to open if possible.
-
-        Returns
-        -------
-        None
-
-        """
-        super(SMOTEENN, self).__init__(ratio=ratio, random_state=random_state,
+        super(SMOTEENN, self).__init__(ratio=ratio,
                                        verbose=verbose)
-
+        self.random_state = random_state
         self.k = k
         self.m = m
         self.out_step = out_step
         self.kind_smote = kind_smote
+        self.size_ngh = size_ngh
+        self.kind_enn = kind_enn
         self.n_jobs = n_jobs
         self.kwargs = kwargs
-
         self.sm = SMOTE(ratio=self.ratio, random_state=self.random_state,
                         verbose=self.verbose, k=self.k, m=self.m,
                         out_step=self.out_step, kind=self.kind_smote,
                         n_jobs=self.n_jobs, **self.kwargs)
-
-        self.size_ngh = size_ngh
-        self.kind_enn = kind_enn
-
         self.enn = EditedNearestNeighbours(random_state=self.random_state,
                                            verbose=self.verbose,
                                            size_ngh=self.size_ngh,
@@ -192,8 +132,6 @@ class SMOTEENN(SamplerMixin):
             Return self.
 
         """
-        # Check the consistency of X and y
-        X, y = check_X_y(X, y)
 
         super(SMOTEENN, self).fit(X, y)
 
@@ -202,7 +140,7 @@ class SMOTEENN(SamplerMixin):
 
         return self
 
-    def sample(self, X, y):
+    def _sample(self, X, y):
         """Resample the dataset.
 
         Parameters
@@ -222,10 +160,6 @@ class SMOTEENN(SamplerMixin):
             The corresponding label of `X_resampled`
 
         """
-        # Check the consistency of X and y
-        X, y = check_X_y(X, y)
-
-        super(SMOTEENN, self).sample(X, y)
 
         # Transform using SMOTE
         X, y = self.sm.sample(X, y)
