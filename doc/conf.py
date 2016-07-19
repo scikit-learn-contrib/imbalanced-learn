db8e76ec53e4028845d3a33351245f3615ace32b
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-07-19 14:17:57 +0200

    bumpversion 0.1.1 -> 0.1.2

--- doc/conf.py
+++ doc/conf.py
@@ -87,7 +87,7 @@ copyright = u'2016, G. Lemaitre, F. Nogueira, D. Oliveira, C. Aridas'
 # built documents.
 #
 # The short X.Y version.
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 version = __version__
 # The full version, including alpha/beta/rc tags.
 release = __version__
