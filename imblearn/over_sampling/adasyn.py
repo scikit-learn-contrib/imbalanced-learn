da02bbc66c17b28232f22c285ffe6a91437da983
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-07-19 15:50:20 +0200

    Update the docstring

--- imblearn/over_sampling/adasyn.py
+++ imblearn/over_sampling/adasyn.py
@@ -64,16 +64,18 @@ class ADASYN(SamplerMixin):
     --------
 
     >>> from collections import Counter
-    >>> from sklearn.datasets import fetch_mldata
-    >>> from imblearn.over_sampling import ADASYN
-    >>> pima = fetch_mldata('diabetes_scale')
-    >>> X, y = pima['data'], pima['target']
+    >>> from sklearn.datasets import make_classification
+    >>> from imblearn.under_sampling import TomekLinks
+    >>> X, y = make_classification(n_classes=2, class_sep=2, weights=[0.1, 0.9],
+    ...                            n_informative=3, n_redundant=1, flip_y=0,
+    ...                            n_features=20, n_clusters_per_class=1,
+    ...                            n_samples=1000, random_state=10)
     >>> print('Original dataset shape {}'.format(Counter(y)))
-    Original dataset shape Counter({1: 500, -1: 268})
-    >>> ada = ADASYN(random_state=42)
-    >>> X_res, y_res = ada.fit_sample(X, y)
+    Original dataset shape Counter({1: 900, 0: 100})
+    >>> tl = TomekLinks(random_state=42)
+    >>> X_res, y_res = tl.fit_sample(X, y)
     >>> print('Resampled dataset shape {}'.format(Counter(y_res)))
-    Resampled dataset shape Counter({1: 500, -1: 478})
+    Resampled dataset shape Counter({1: 897, 0: 100})
 
     References
     ----------
--- imblearn/under_sampling/edited_nearest_neighbours.py
+++ imblearn/under_sampling/edited_nearest_neighbours.py
@@ -73,16 +73,18 @@ class EditedNearestNeighbours(SamplerMixin):
     --------
 
     >>> from collections import Counter
-    >>> from sklearn.datasets import fetch_mldata
+    >>> from sklearn.datasets import make_classification
     >>> from imblearn.under_sampling import EditedNearestNeighbours
-    >>> pima = fetch_mldata('diabetes_scale')
-    >>> X, y = pima['data'], pima['target']
+    >>> X, y = make_classification(n_classes=2, class_sep=2, weights=[0.1, 0.9],
+    ...                            n_informative=3, n_redundant=1, flip_y=0,
+    ...                            n_features=20, n_clusters_per_class=1,
+    ...                            n_samples=1000, random_state=10)
     >>> print('Original dataset shape {}'.format(Counter(y)))
-    Original dataset shape Counter({1: 500, -1: 268})
+    Original dataset shape Counter({1: 900, 0: 100})
     >>> enn = EditedNearestNeighbours(random_state=42)
     >>> X_res, y_res = enn.fit_sample(X, y)
     >>> print('Resampled dataset shape {}'.format(Counter(y_res)))
-    Resampled dataset shape Counter({1: 279, -1: 268})
+    Resampled dataset shape Counter({1: 883, 0: 100})
 
     References
     ----------
@@ -259,16 +261,18 @@ class RepeatedEditedNearestNeighbours(SamplerMixin):
     --------
 
     >>> from collections import Counter
-    >>> from sklearn.datasets import fetch_mldata
+    >>> from sklearn.datasets import make_classification
     >>> from imblearn.under_sampling import RepeatedEditedNearestNeighbours
-    >>> pima = fetch_mldata('diabetes_scale')
-    >>> X, y = pima['data'], pima['target']
+    >>> X, y = make_classification(n_classes=2, class_sep=2, weights=[0.1, 0.9],
+    ...                            n_informative=3, n_redundant=1, flip_y=0,
+    ...                            n_features=20, n_clusters_per_class=1,
+    ...                            n_samples=1000, random_state=10)
     >>> print('Original dataset shape {}'.format(Counter(y)))
-    Original dataset shape Counter({1: 500, -1: 268})
+    Original dataset shape Counter({1: 900, 0: 100})
     >>> renn = RepeatedEditedNearestNeighbours(random_state=42)
     >>> X_res, y_res = renn.fit_sample(X, y)
     >>> print('Resampled dataset shape {}'.format(Counter(y_res)))
-    Resampled dataset shape Counter({1: 227, -1: 200})
+    Resampled dataset shape Counter({1: 883, 0: 100})
 
     References
     ----------
