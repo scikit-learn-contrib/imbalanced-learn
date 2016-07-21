be2d8645359ae3d2facc15496df7625cfb611af6
Dayvid Victor <victor.dvro@gmail.com>
2016-07-21 00:07:08 -0300

    changing allknn doctest and removing internal data copy in _sample(X, y)

--- imblearn/under_sampling/edited_nearest_neighbours.py
+++ imblearn/under_sampling/edited_nearest_neighbours.py
@@ -439,16 +439,18 @@ class AllKNN(SamplerMixin):
     --------
 
     >>> from collections import Counter
-    >>> from sklearn.datasets import fetch_mldata
+    >>> from sklearn.datasets import make_classification
     >>> from imblearn.under_sampling import AllKNN
-    >>> pima = fetch_mldata('diabetes_scale')
-    >>> X, y = pima['data'], pima['target']
+    >>> X, y = make_classification(n_classes=2, class_sep=2, weights=[0.1, 0.9],
+    ...                            n_informative=3, n_redundant=1, flip_y=0,
+    ...                            n_features=20, n_clusters_per_class=1,
+    ...                            n_samples=1000, random_state=10)
     >>> print('Original dataset shape {}'.format(Counter(y)))
-    Original dataset shape Counter({1: 500, -1: 268})
+    Original dataset shape Counter({1: 900, 0: 100})
     >>> allknn = AllKNN(random_state=42)
     >>> X_res, y_res = allknn.fit_sample(X, y)
     >>> print('Resampled dataset shape {}'.format(Counter(y_res)))
-    Resampled dataset shape Counter({-1: 268, 1: 249})
+    Resampled dataset shape Counter({1: 883, 0: 100})
 
     References
     ----------
@@ -523,7 +525,7 @@ class AllKNN(SamplerMixin):
         if self.kind_sel not in SEL_KIND:
             raise NotImplementedError
 
-        X_, y_ = X.copy(), y.copy()
+        X_, y_ = X, y
 
         if self.return_indices:
             idx_under = np.arange(X.shape[0], dtype=int)
