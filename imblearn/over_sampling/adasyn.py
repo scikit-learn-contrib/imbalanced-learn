a97f66b0bcbce45b5062d1d03c3f23d4eee1c12d
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2016-07-26 00:35:30 +0200

    Address issue #107 - ADASYN docstring (#108)

--- imblearn/over_sampling/adasyn.py
+++ imblearn/over_sampling/adasyn.py
@@ -65,17 +65,17 @@ class ADASYN(SamplerMixin):
 
     >>> from collections import Counter
     >>> from sklearn.datasets import make_classification
-    >>> from imblearn.under_sampling import TomekLinks
+    >>> from imblearn.over_sampling import ADASYN
     >>> X, y = make_classification(n_classes=2, class_sep=2, weights=[0.1, 0.9],
     ...                            n_informative=3, n_redundant=1, flip_y=0,
     ...                            n_features=20, n_clusters_per_class=1,
     ...                            n_samples=1000, random_state=10)
     >>> print('Original dataset shape {}'.format(Counter(y)))
     Original dataset shape Counter({1: 900, 0: 100})
-    >>> tl = TomekLinks(random_state=42)
-    >>> X_res, y_res = tl.fit_sample(X, y)
+    >>> ada = ADASYN(random_state=42)
+    >>> X_res, y_res = ada.fit_sample(X, y)
     >>> print('Resampled dataset shape {}'.format(Counter(y_res)))
-    Resampled dataset shape Counter({1: 897, 0: 100})
+    Resampled dataset shape Counter({0: 909, 1: 900})
 
     References
     ----------
