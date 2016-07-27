0b9b5ac8437e9f25f9aa5657e4b2163997789481
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-07-27 14:40:09 +0200

    Check multiclass for CondensedNearestNeighbour

--- imblearn/under_sampling/condensed_nearest_neighbour.py
+++ imblearn/under_sampling/condensed_nearest_neighbour.py
@@ -3,11 +3,14 @@ method."""
 from __future__ import print_function
 from __future__ import division
 
+import warnings
+
 import numpy as np
 
 from collections import Counter
 
 from sklearn.utils import check_random_state
+from sklearn.utils.multiclass import type_of_target
 from sklearn.neighbors import KNeighborsClassifier
 
 from ..base import SamplerMixin
@@ -97,6 +100,33 @@ class CondensedNearestNeighbour(SamplerMixin):
         self.n_seeds_S = n_seeds_S
         self.n_jobs = n_jobs
         self.kwargs = kwargs
+
+    def fit(self, X, y):
+        """Find the classes statistics before to perform sampling.
+
+        Parameters
+        ----------
+        X : ndarray, shape (n_samples, n_features)
+            Matrix containing the data which have to be sampled.
+
+        y : ndarray, shape (n_samples, )
+            Corresponding label for each sample in X.
+
+        Returns
+        -------
+        self : object,
+            Return self.
+
+        """
+
+        super(CondensedNearestNeighbour, self).fit(X, y)
+
+        # Check that y is binary
+        if not (type_of_target(y) == 'binary' or
+                type_of_target(y) == 'multiclass'):
+            warnings.warn('The target type should be binary or multiclass.')
+
+        return self
 
     def _sample(self, X, y):
         """Resample the dataset.
--- imblearn/under_sampling/tests/test_condensed_nearest_neighbour.py
+++ imblearn/under_sampling/tests/test_condensed_nearest_neighbour.py
@@ -12,6 +12,8 @@ from numpy.testing import assert_warns
 from sklearn.datasets import make_classification
 from sklearn.utils.estimator_checks import check_estimator
 
+from collections import Counter
+
 from imblearn.under_sampling import CondensedNearestNeighbour
 
 # Generate a global dataset to use
@@ -112,3 +114,31 @@ def test_cnn_sample_wrong_X():
     cnn.fit(X, Y)
     assert_raises(RuntimeError, cnn.sample, np.random.random((100, 40)),
                   np.array([0] * 50 + [1] * 50))
+
+
+def test_continuous_error():
+    """Test either if an error is raised when the target are continuous
+    type"""
+
+    # continuous case
+    y = np.linspace(0, 1, 5000)
+    cnn = CondensedNearestNeighbour(random_state=RND_SEED)
+    assert_warns(UserWarning, cnn.fit, X, y)
+
+
+def test_multiclass_fit_sample():
+    """Test fit sample method with multiclass target"""
+
+    # Make y to be multiclass
+    y = Y.copy()
+    y[0:1000] = 2
+
+    # Resample the data
+    cnn = CondensedNearestNeighbour(random_state=RND_SEED)
+    X_resampled, y_resampled = cnn.fit_sample(X, y)
+
+    # Check the size of y
+    count_y_res = Counter(y_resampled)
+    assert_equal(count_y_res[0], 400)
+    assert_equal(count_y_res[1], 113)
+    assert_equal(count_y_res[2], 147)
