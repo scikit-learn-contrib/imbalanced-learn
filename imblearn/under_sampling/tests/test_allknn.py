6e318d1f5d9a981d4c0807f4f0b1a0cd4d134ca6
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-08-31 12:14:23 +0200

    Toy testing for AllKNN

--- imblearn/under_sampling/tests/test_allknn.py
+++ imblearn/under_sampling/tests/test_allknn.py
@@ -19,10 +19,48 @@ from imblearn.under_sampling import AllKNN
 
 # Generate a global dataset to use
 RND_SEED = 0
-X, Y = make_classification(n_classes=2, class_sep=2, weights=[0.1, 0.9],
-                           n_informative=3, n_redundant=1, flip_y=0,
-                           n_features=20, n_clusters_per_class=1,
-                           n_samples=5000, random_state=RND_SEED)
+X = np.array([[-0.12840393, 0.66446571],
+              [1.32319756, -0.13181616],
+              [0.04296502, -0.37981873],
+              [0.83631853, 0.18569783],
+              [1.02956816, 0.36061601],
+              [1.12202806, 0.33811558],
+              [-0.53171468, -0.53735182],
+              [1.3381556, 0.35956356],
+              [-0.35946678, 0.72510189],
+              [1.32326943, 0.28393874],
+              [2.94290565, -0.13986434],
+              [0.28294738, -1.00125525],
+              [0.34218094, -0.58781961],
+              [-0.88864036, -0.33782387],
+              [-1.10146139, 0.91782682],
+              [-0.7969716, -0.50493969],
+              [0.73489726, 0.43915195],
+              [0.2096964, -0.61814058],
+              [-0.28479268, 0.70459548],
+              [1.84864913, 0.14729596],
+              [1.59068979, -0.96622933],
+              [0.73418199, -0.02222847],
+              [0.50307437, 0.498805],
+              [0.84929742, 0.41042894],
+              [0.62649535, 0.46600596],
+              [0.79270821, -0.41386668],
+              [1.16606871, -0.25641059],
+              [1.57356906, 0.30390519],
+              [1.0304995, -0.16955962],
+              [1.67314371, 0.19231498],
+              [0.98382284, 0.37184502],
+              [0.48921682, -1.38504507],
+              [-0.46226554, -0.50481004],
+              [-0.03918551, -0.68540745],
+              [0.24991051, -1.00864997],
+              [0.80541964, -0.34465185],
+              [0.1732627, -1.61323172],
+              [0.69804044, 0.44810796],
+              [-0.5506368, -0.42072426],
+              [-0.34474418, 0.21969797]])
+Y = np.array([1, 2, 2, 2, 1, 1, 0, 2, 1, 1, 1, 2, 2, 0, 1, 2, 1, 2, 1, 1, 2,
+              2, 1, 1, 1, 2, 2, 2, 2, 1, 1, 2, 0, 2, 2, 2, 2, 1, 2, 0])
 
 
 def test_allknn_sk_estimator():
@@ -63,10 +101,10 @@ def test_allknn_fit():
 
     # Check if the data information have been computed
     assert_equal(allknn.min_c_, 0)
-    assert_equal(allknn.maj_c_, 1)
-    assert_equal(allknn.stats_c_[0], 500)
-    assert_equal(allknn.stats_c_[1], 4500)
-
+    assert_equal(allknn.maj_c_, 2)
+    assert_equal(allknn.stats_c_[0], 4)
+    assert_equal(allknn.stats_c_[1], 16)
+    assert_equal(allknn.stats_c_[2], 20)
 
 def test_allknn_sample_wt_fit():
     """Test either if an error is raised when sample is called before
@@ -84,9 +122,35 @@ def test_allknn_fit_sample():
     allknn = AllKNN(random_state=RND_SEED)
     X_resampled, y_resampled = allknn.fit_sample(X, Y)
 
-    currdir = os.path.dirname(os.path.abspath(__file__))
-    X_gt = np.load(os.path.join(currdir, 'data', 'allknn_x.npy'))
-    y_gt = np.load(os.path.join(currdir, 'data', 'allknn_y.npy'))
+    X_gt = np.array([[-0.53171468, -0.53735182],
+                     [-0.88864036, -0.33782387],
+                     [-0.46226554, -0.50481004],
+                     [-0.34474418, 0.21969797],
+                     [1.02956816, 0.36061601],
+                     [1.12202806, 0.33811558],
+                     [-1.10146139, 0.91782682],
+                     [0.73489726, 0.43915195],
+                     [0.50307437, 0.498805],
+                     [0.84929742, 0.41042894],
+                     [0.62649535, 0.46600596],
+                     [0.98382284, 0.37184502],
+                     [0.69804044, 0.44810796],
+                     [0.04296502, -0.37981873],
+                     [0.28294738, -1.00125525],
+                     [0.34218094, -0.58781961],
+                     [0.2096964, -0.61814058],
+                     [1.59068979, -0.96622933],
+                     [0.73418199, -0.02222847],
+                     [0.79270821, -0.41386668],
+                     [1.16606871, -0.25641059],
+                     [1.0304995, -0.16955962],
+                     [0.48921682, -1.38504507],
+                     [-0.03918551, -0.68540745],
+                     [0.24991051, -1.00864997],
+                     [0.80541964, -0.34465185],
+                     [0.1732627, -1.61323172]])
+    y_gt = np.array([0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2,
+                     2, 2, 2, 2, 2, 2, 2, 2])
     assert_array_almost_equal(X_resampled, X_gt)
     assert_array_almost_equal(y_resampled, y_gt)
 
@@ -98,10 +162,38 @@ def test_allknn_fit_sample_with_indices():
     allknn = AllKNN(return_indices=True, random_state=RND_SEED)
     X_resampled, y_resampled, idx_under = allknn.fit_sample(X, Y)
 
-    currdir = os.path.dirname(os.path.abspath(__file__))
-    X_gt = np.load(os.path.join(currdir, 'data', 'allknn_x.npy'))
-    y_gt = np.load(os.path.join(currdir, 'data', 'allknn_y.npy'))
-    idx_gt = np.load(os.path.join(currdir, 'data', 'allknn_idx.npy'))
+
+    X_gt = np.array([[-0.53171468, -0.53735182],
+                     [-0.88864036, -0.33782387],
+                     [-0.46226554, -0.50481004],
+                     [-0.34474418, 0.21969797],
+                     [1.02956816, 0.36061601],
+                     [1.12202806, 0.33811558],
+                     [-1.10146139, 0.91782682],
+                     [0.73489726, 0.43915195],
+                     [0.50307437, 0.498805],
+                     [0.84929742, 0.41042894],
+                     [0.62649535, 0.46600596],
+                     [0.98382284, 0.37184502],
+                     [0.69804044, 0.44810796],
+                     [0.04296502, -0.37981873],
+                     [0.28294738, -1.00125525],
+                     [0.34218094, -0.58781961],
+                     [0.2096964, -0.61814058],
+                     [1.59068979, -0.96622933],
+                     [0.73418199, -0.02222847],
+                     [0.79270821, -0.41386668],
+                     [1.16606871, -0.25641059],
+                     [1.0304995, -0.16955962],
+                     [0.48921682, -1.38504507],
+                     [-0.03918551, -0.68540745],
+                     [0.24991051, -1.00864997],
+                     [0.80541964, -0.34465185],
+                     [0.1732627, -1.61323172]])
+    y_gt = np.array([0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2,
+                     2, 2, 2, 2, 2, 2, 2, 2])
+    idx_gt = np.array([6, 13, 32, 39, 4, 5, 14, 16, 22, 23, 24, 30, 37, 2,
+                       11, 12, 17, 20, 21, 25, 26, 28, 31, 33, 34, 35, 36])
     assert_array_almost_equal(X_resampled, X_gt)
     assert_array_almost_equal(y_resampled, y_gt)
     assert_array_almost_equal(idx_under, idx_gt)
@@ -114,11 +206,41 @@ def test_allknn_fit_sample_mode():
     allknn = AllKNN(random_state=RND_SEED, kind_sel='mode')
     X_resampled, y_resampled = allknn.fit_sample(X, Y)
 
-    currdir = os.path.dirname(os.path.abspath(__file__))
-    X_gt = np.load(os.path.join(currdir, 'data', 'allknn_x_mode.npy'))
-    y_gt = np.load(os.path.join(currdir, 'data', 'allknn_y_mode.npy'))
+    X_gt = np.array([[-0.53171468, -0.53735182],
+                     [-0.88864036, -0.33782387],
+                     [-0.46226554, -0.50481004],
+                     [-0.34474418, 0.21969797],
+                     [-0.12840393, 0.66446571],
+                     [1.02956816, 0.36061601],
+                     [1.12202806, 0.33811558],
+                     [-0.35946678, 0.72510189],
+                     [-1.10146139, 0.91782682],
+                     [0.73489726, 0.43915195],
+                     [-0.28479268, 0.70459548],
+                     [0.50307437, 0.498805],
+                     [0.84929742, 0.41042894],
+                     [0.62649535, 0.46600596],
+                     [0.98382284, 0.37184502],
+                     [0.69804044, 0.44810796],
+                     [1.32319756, -0.13181616],
+                     [0.04296502, -0.37981873],
+                     [0.28294738, -1.00125525],
+                     [0.34218094, -0.58781961],
+                     [0.2096964, -0.61814058],
+                     [1.59068979, -0.96622933],
+                     [0.73418199, -0.02222847],
+                     [0.79270821, -0.41386668],
+                     [1.16606871, -0.25641059],
+                     [1.0304995, -0.16955962],
+                     [0.48921682, -1.38504507],
+                     [-0.03918551, -0.68540745],
+                     [0.24991051, -1.00864997],
+                     [0.80541964, -0.34465185],
+                     [0.1732627, -1.61323172]])
+    y_gt = np.array([0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2,
+                     2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2])
     assert_array_equal(X_resampled, X_gt)
-    assert_array_almost_equal(y_resampled, y_gt)
+    assert_array_equal(y_resampled, y_gt)
 
 
 def test_allknn_sample_wrong_X():
@@ -137,24 +259,6 @@ def test_continuous_error():
     type"""
 
     # continuous case
-    y = np.linspace(0, 1, 5000)
+    y = np.linspace(0, 1, 40)
     ann = AllKNN(random_state=RND_SEED)
     assert_warns(UserWarning, ann.fit, X, y)
-
-
-def test_multiclass_fit_sample():
-    """Test fit sample method with multiclass target"""
-
-    # Make y to be multiclass
-    y = Y.copy()
-    y[0:1000] = 2
-
-    # Resample the data
-    ann = AllKNN(random_state=RND_SEED)
-    X_resampled, y_resampled = ann.fit_sample(X, y)
-
-    # Check the size of y
-    count_y_res = Counter(y_resampled)
-    assert_equal(count_y_res[0], 400)
-    assert_equal(count_y_res[1], 3600)
-    assert_equal(count_y_res[2], 1000)
