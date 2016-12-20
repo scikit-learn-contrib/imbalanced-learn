a78902da34f7d1cbeba21e40df2bb7bc101becc0
chkoar <ichkoar@gmail.com>
2016-12-21 00:21:32 +0200

    pep8

--- examples/model_selection/plot_validation_curve.py
+++ examples/model_selection/plot_validation_curve.py
@@ -3,11 +3,11 @@
 Plotting Validation Curves
 ==========================
 
-In this example the impact of the SMOTE's k_neighbors parameter is examined. 
-In the plot you can see the validation scores of a SMOTE-CART classifier for 
-different values of the SMOTE's k_neighbors parameter. 
+In this example the impact of the SMOTE's k_neighbors parameter is examined.
+In the plot you can see the validation scores of a SMOTE-CART classifier for
+different values of the SMOTE's k_neighbors parameter.
 """
-print(__doc__)
+
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -17,19 +17,22 @@ from sklearn import datasets, metrics, tree
 from imblearn import over_sampling as os
 from imblearn import pipeline as pl
 
+print(__doc__)
+
 LW = 2
 RANDOM_STATE = 42
 
 scorer = metrics.make_scorer(metrics.cohen_kappa_score)
 
 # Generate the dataset
-X, y = datasets.make_classification(n_classes=2, class_sep=2, weights=[0.1, 0.9],
-                                    n_informative=10, n_redundant=1, flip_y=0,
-                                    n_features=20, n_clusters_per_class=4,
-                                    n_samples=5000, random_state=10)
-
-pipeline = pl.make_pipeline(os.SMOTE(
-    random_state=RANDOM_STATE), tree.DecisionTreeClassifier(random_state=RANDOM_STATE))
+X, y = datasets.make_classification(n_classes=2, class_sep=2,
+                                    weights=[0.1, 0.9], n_informative=10,
+                                    n_redundant=1, flip_y=0, n_features=20,
+                                    n_clusters_per_class=4, n_samples=5000,
+                                    random_state=10)
+smote = os.SMOTE(random_state=RANDOM_STATE)
+cart = tree.DecisionTreeClassifier(random_state=RANDOM_STATE)
+pipeline = pl.make_pipeline(smote, cart)
 
 param_range = range(1, 11)
 train_scores, test_scores = ms.validation_curve(
