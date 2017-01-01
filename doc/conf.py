164c4b1d02954114601f5640e80b0c52dd65a376
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2017-01-01 16:41:47 +0100

    release 0.2.1

--- doc/conf.py
+++ doc/conf.py
@@ -87,7 +87,7 @@ copyright = u'2016, G. Lemaitre, F. Nogueira, D. Oliveira, C. Aridas'
 # built documents.
 #
 # The short X.Y version.
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 version = __version__
 # The full version, including alpha/beta/rc tags.
 release = __version__
