5fc42f82f579095ef5f140bfd499f76c408e18fe
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-03-12 13:14:12 +0100

    [MRG] CI moving to codecov (#239)
    * CI moving to codecov
    
    * [MRG] FIX comment fix (#5)
    
    * CI rename coverage folder and remove flake8 test
    
    * CI/FIX fixing coverage
    
    * CI/FIX correct codecov
    
    * CI/FIX no need for timer
    
    * CI/FIX check which folder containt the coverage
    
    * CI/FIX check which folder containt the coverage
    
    * CI/FIX avoid copying one directory

--- build_tools/travis/after_success.sh
+++ build_tools/travis/after_success.sh
@@ -6,14 +6,11 @@
 
 set -e
 
-if [[ "$COVERAGE" == "true" ]]; then
-    # Need to run coveralls from a git checkout, so we copy .coverage
-    # from TEST_DIR where nosetests has been run
-    cp $TEST_DIR/.coverage $TRAVIS_BUILD_DIR
-    cd $TRAVIS_BUILD_DIR
-    # Ignore coveralls failures as the coveralls server is not
-    # very reliable but we don't want travis to report a failure
-    # in the github UI just because the coverage report failed to
-    # be published.
-    coveralls || echo "Coveralls upload failed"
-fi
+# Need to run covdecov from a git checkout, so we copy .coverage
+# from TEST_DIR where nosetests has been run
+cd $TRAVIS_BUILD_DIR
+# Ignore covdecov failures as the covdecov server is not
+# very reliable but we don't want travis to report a failure
+# in the github UI just because the coverage report failed to
+# be published.
+codecov || echo "Covdecov upload failed"
