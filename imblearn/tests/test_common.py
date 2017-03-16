e7c2375ede71399f258bb98293fc8027cf91e47a
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2017-03-16 18:50:38 +0100

    TST add test for meta-classifiers

--- imblearn/tests/test_common.py
+++ imblearn/tests/test_common.py
@@ -1,8 +1,17 @@
 from sklearn.utils.testing import assert_greater
+from sklearn.utils.testing import assert_false
 from sklearn.utils.testing import _named_check
-from sklearn.utils.estimator_checks import check_estimator
+from sklearn.utils.estimator_checks import check_estimator, _yield_all_checks
 
 from imblearn.utils.testing import all_estimators
+
+
+def test_all_estimator_no_base_class():
+    # test that all_estimators doesn't find abstract classes.
+    for name, Estimator in all_estimators():
+        msg = ("Base estimators such as {0} should not be included"
+               " in all_estimators").format(name)
+        assert_false(name.lower().startswith('base'), msg=msg)
 
 
 def test_all_estimators():
@@ -12,3 +21,13 @@ def test_all_estimators():
         # some can just not be sensibly default constructed
         yield (_named_check(check_estimator, name),
                Estimator)
+
+
+def test_non_meta_estimators():
+    # input validation etc for non-meta estimators
+    estimators = all_estimators()
+    for name, Estimator in estimators:
+        if name.startswith("_"):
+            continue
+        for check in _yield_all_checks(name, Estimator):
+            yield _named_check(check, name), name, Estimator
