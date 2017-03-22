a951029bd99d7e5d6a92b3d2a3d2f00ecd2f90c0
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-03-22 13:30:47 +0100

    DOC/EXA solve Tomek examples (#263)

--- examples/under-sampling/plot_tomek_links.py
+++ examples/under-sampling/plot_tomek_links.py
@@ -4,48 +4,56 @@ Tomek links
 ===========
 
 An illustration of the Tomek links method.
-
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-from sklearn.datasets import make_blobs
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 
 from imblearn.under_sampling import TomekLinks
 
 print(__doc__)
 
-
-# create a synthetic dataset
-X, y = make_blobs(n_samples=500, centers=2, n_features=2,
-                  random_state=0, center_box=(-5.0, 5.0))
+rng = np.random.RandomState(0)
+n_samples_1 = 500
+n_samples_2 = 50
+X_syn = np.r_[1.5 * rng.randn(n_samples_1, 2),
+              0.5 * rng.randn(n_samples_2, 2) + [2, 2]]
+y_syn = np.array([0] * (n_samples_1) + [1] * (n_samples_2))
+X_syn, y_syn = shuffle(X_syn, y_syn)
+X_syn_train, X_syn_test, y_syn_train, y_syn_test = train_test_split(X_syn,
+                                                                    y_syn)
 
 # remove Tomek links
 tl = TomekLinks(return_indices=True)
-X_resampled, y_resampled, idx_resampled = tl.fit_sample(X, y)
+X_resampled, y_resampled, idx_resampled = tl.fit_sample(X_syn, y_syn)
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 
-idx_class_0 = np.flatnonzero(y_resampled == 0)
-idx_class_1 = np.flatnonzero(y_resampled == 1)
-idx_samples_removed = np.setdiff1d(np.flatnonzero(y == 1),
-                                   np.union1d(idx_class_0, idx_class_1))
+idx_samples_removed = np.setdiff1d(np.arange(X_syn.shape[0]),
+                                   idx_resampled)
+idx_class_0 = y_resampled == 0
+plt.scatter(X_resampled[idx_class_0, 0], X_resampled[idx_class_0, 1],
+            alpha=.8, label='Class #0')
+plt.scatter(X_resampled[~idx_class_0, 0], X_resampled[~idx_class_0, 1],
+            alpha=.8, label='Class #1')
+plt.scatter(X_syn[idx_samples_removed, 0], X_syn[idx_samples_removed, 1],
+            alpha=.8, label='Removed samples')
 
-plt.scatter(X[idx_class_0, 0], X[idx_class_0, 1],
-            c='g', alpha=.8, label='Class #0')
-plt.scatter(X[idx_class_1, 0], X[idx_class_1, 1],
-            c='b', alpha=.8, label='Class #1')
-plt.scatter(X[idx_samples_removed, 0], X[idx_samples_removed, 1],
-            c='r', alpha=.8, label='Removed samples')
-
+# make nice plotting
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 ax.get_xaxis().tick_bottom()
 ax.get_yaxis().tick_left()
 ax.spines['left'].set_position(('outward', 10))
 ax.spines['bottom'].set_position(('outward', 10))
+ax.set_xlim([-5, 5])
+ax.set_ylim([-5, 5])
+plt.yticks(range(-5, 6))
+plt.xticks(range(-5, 6))
 
 plt.title('Under-sampling removing Tomek links')
 plt.legend()
