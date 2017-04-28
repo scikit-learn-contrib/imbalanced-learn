24b9be9ebb4c2d9b5a3f5aa02b746daee4d033d4
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-04-28 14:51:56 +0200

    FIX fix setup (#286)

--- imblearn/setup.py
+++ imblearn/setup.py
@@ -5,6 +5,8 @@ def configuration(parent_package='', top_path=None):
 
     config.add_subpackage('combine')
     config.add_subpackage('combine/tests')
+    config.add_subpackage('datasets')
+    config.add_subpackage('datasets/tests')
     config.add_subpackage('ensemble')
     config.add_subpackage('ensemble/tests')
     config.add_subpackage('metrics')
