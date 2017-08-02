69c9719453d6d8e532edda3a3b51436f7b6158df
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-08-02 15:15:24 +0200

    CI add matrix for travis

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -1,45 +1,66 @@
-# Deactivate the travis-provided virtual environment and setup a
-# conda-based environment instead
-deactivate
+#!/bin/bash
+# This script is meant to be called by the "install" step defined in
+# .travis.yml. See http://docs.travis-ci.com/ for more details.
+# The behavior of the script is controlled by environment variabled defined
+# in the .travis.yml in the top level folder of the project.
 
-# Use the miniconda installer for faster download / install of conda
-# itself
-pushd .
-cd
-mkdir -p download
-cd download
-echo "Cached in $HOME/download :"
-ls -l
-echo
-if [[ ! -f miniconda.sh ]]
-   then
-   wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh \
+# License: 3-clause BSD
+
+# Travis clone pydicom/pydicom repository in to a local repository.
+
+set -e
+
+echo 'List files from cached directories'
+echo 'pip:'
+ls $HOME/.cache/pip
+
+export CC=/usr/lib/ccache/gcc
+export CXX=/usr/lib/ccache/g++
+# Useful for debugging how ccache is used
+# export CCACHE_LOGFILE=/tmp/ccache.log
+# ~60M is used by .ccache when compiling from scratch at the time of writing
+ccache --max-size 100M --show-stats
+
+if [[ "$DISTRIB" == "conda" ]]; then
+    # Deactivate the travis-provided virtual environment and setup a
+    # conda-based environment instead
+    deactivate
+
+    # Install miniconda
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
         -O miniconda.sh
-   fi
-chmod +x miniconda.sh && ./miniconda.sh -b
-cd ..
-export PATH=/home/travis/miniconda2/bin:$PATH
-conda update --yes conda
-popd
+    MINICONDA_PATH=/home/travis/miniconda
+    chmod +x miniconda.sh && ./miniconda.sh -b -p $MINICONDA_PATH
+    export PATH=$MINICONDA_PATH/bin:$PATH
+    conda update --yes conda
 
-# Configure the conda environment and put it in the path using the
-# provided versions
-conda create -n testenv --yes python=$PYTHON_VERSION pip nose
-    numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION \
-    libgfortran nomkl
-source activate testenv
+    # Configure the conda environment and put it in the path using the
+    # provided versions
+    conda create -n testenv --yes python=$PYTHON_VERSION pip
+    source activate testenv
+    conda install --yes numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION \
+          scikit-learn=$SKLEARN_VERSION
+    conda install --yes nose pytest pytest-cov
+    # Install nose-timer via pip
+    pip install nose-timer codecov
 
-# Install scikit-learn
-conda install --yes scikit-learn=$SKLEARN_VERSION
+elif [[ "$DISTRIB" == "ubuntu" ]]; then
+    # At the time of writing numpy 1.9.1 is included in the travis
+    # virtualenv but we want to use the numpy installed through apt-get
+    # install.
+    deactivate
+    # Create a new virtualenv using system site packages for python, numpy
+    virtualenv --system-site-packages testvenv
+    source testvenv/bin/activate
+    pip install scikit-learn nose nose-timer pytest pytest-cov codecov
 
-# Install nose-timer via pip
-pip install nose-timer coverage codecov
+fi
 
-# Build imbalanced-learn in the install.sh script to collapse the verbose
-# build output in the travis output when it succeeds.
 python --version
 python -c "import numpy; print('numpy %s' % numpy.__version__)"
 python -c "import scipy; print('scipy %s' % scipy.__version__)"
 
-cd $TRAVIS_BUILD_DIR
 python setup.py develop
+ccache --show-stats
+# Useful for debugging how ccache is used
+# cat $CCACHE_LOGFILE
