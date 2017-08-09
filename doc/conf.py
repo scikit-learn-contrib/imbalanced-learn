25163b086ed72e7c180c726f43c5f6f4256b03f9
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-08-10 00:20:21 +0200

    DOC add next previous buttons back

--- doc/conf.py
+++ doc/conf.py
@@ -158,7 +158,7 @@ html_theme = 'sphinx_rtd_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {'prev_next_buttons_location': None}
+# html_theme_options = {'prev_next_buttons_location': None}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
