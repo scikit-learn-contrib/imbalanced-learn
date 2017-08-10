07e95e41b60f2752fa66d6439795456e1e950d51
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-08-10 12:28:15 +0200

    EHN add BalancedBaggingClassifier

--- imblearn/ensemble/__init__.py
+++ imblearn/ensemble/__init__.py
@@ -3,7 +3,7 @@ The :mod:`imblearn.ensemble` module include methods generating
 under-sampled subsets combined inside an ensemble.
 """
 
-from .easy_ensemble import EasyEnsemble
+from .easy_ensemble import EasyEnsemble, BalancedBaggingClassifier
 from .balance_cascade import BalanceCascade
 
-__all__ = ['EasyEnsemble', 'BalanceCascade']
+__all__ = ['EasyEnsemble', 'BalancedBaggingClassifier', 'BalanceCascade']
