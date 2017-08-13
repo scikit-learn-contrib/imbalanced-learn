bcf44ab3e3d22a3660ba59e50ec2e7933b8036bb
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-08-14 00:12:47 +0200

    EHN support sparse adasyn

--- imblearn/over_sampling/tests/test_adasyn.py
+++ imblearn/over_sampling/tests/test_adasyn.py
@@ -73,34 +73,11 @@ def test_ada_fit_sample():
     assert_array_equal(y_resampled, y_gt)
 
 
-def test_ada_fit_sample_half():
+def test_ada_fit_ratio_error():
     ratio = 0.8
     ada = ADASYN(ratio=ratio, random_state=RND_SEED)
-    X_resampled, y_resampled = ada.fit_sample(X, Y)
-    X_gt = np.array([[0.11622591, -0.0317206],
-                     [0.77481731, 0.60935141],
-                     [1.25192108, -0.22367336],
-                     [0.53366841, -0.30312976],
-                     [1.52091956, -0.49283504],
-                     [-0.28162401, -2.10400981],
-                     [0.83680821, 1.72827342],
-                     [0.3084254, 0.33299982],
-                     [0.70472253, -0.73309052],
-                     [0.28893132, -0.38761769],
-                     [1.15514042, 0.0129463],
-                     [0.88407872, 0.35454207],
-                     [1.31301027, -0.92648734],
-                     [-1.11515198, -0.93689695],
-                     [-0.18410027, -0.45194484],
-                     [0.9281014, 0.53085498],
-                     [-0.14374509, 0.27370049],
-                     [-0.41635887, -0.38299653],
-                     [0.08711622, 0.93259929],
-                     [1.70580611, -0.11219234]])
-    y_gt = np.array(
-        [0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0])
-    assert_allclose(X_resampled, X_gt, rtol=R_TOL)
-    assert_array_equal(y_resampled, y_gt)
+    assert_raises_regex(ValueError, "No samples will be generated.",
+                        ada.fit_sample, X, Y)
 
 
 def test_ada_fit_sample_nn_obj():
