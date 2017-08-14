58fa5a1f3f098177ea10e67c755bd4b64f2a214f
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-08-14 21:05:34 +0200

    FIX mv into a new module

--- imblearn/ensemble/__init__.py
+++ imblearn/ensemble/__init__.py
@@ -3,7 +3,9 @@ The :mod:`imblearn.ensemble` module include methods generating
 under-sampled subsets combined inside an ensemble.
 """
 
-from .easy_ensemble import EasyEnsemble, BalancedBaggingClassifier
+from .easy_ensemble import EasyEnsemble
 from .balance_cascade import BalanceCascade
+
+from .classifier import BalancedBaggingClassifier
 
 __all__ = ['EasyEnsemble', 'BalancedBaggingClassifier', 'BalanceCascade']
