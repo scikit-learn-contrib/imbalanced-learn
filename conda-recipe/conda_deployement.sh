455a9c0547a90db3c66ed840b308ca5aad0899cc
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-08-24 23:04:52 +0200

    MAINT conda yaml

--- conda-recipe/conda_deployement.sh
+++ conda-recipe/conda_deployement.sh
@@ -2,7 +2,7 @@
 
 # This script is used to build and upload package to conda cloud
 # in an automatic manner
-for pyv in {2.7,3.4,3.5,3.6} ; do
+for pyv in {2.7,3.5,3.6} ; do
     mkdir build_conda
     conda build --python=$pyv --output-folder build_conda imbalanced-learn
     conda convert --platform all build_conda/*.tar.bz2 \
