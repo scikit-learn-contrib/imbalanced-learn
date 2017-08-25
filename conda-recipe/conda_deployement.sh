4c363d32a38fa7f2af5fc186c692d5534fb67723
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-08-25 16:19:50 +0200

    CI update conda build and check doc

--- conda-recipe/conda_deployement.sh
+++ conda-recipe/conda_deployement.sh
@@ -5,7 +5,7 @@
 for pyv in {2.7,3.5,3.6} ; do
     mkdir build_conda
     conda build --python=$pyv --output-folder build_conda imbalanced-learn
-    conda convert --platform all build_conda/*.tar.bz2 \
+    conda convert --platform all build_conda/linux-64/*.tar.bz2 \
           -o build_conda/
     dirs=`ls -l "$PWD/build_conda"`
     for d in build_conda/*/ ; do
