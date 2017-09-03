4151711ecc50120c29ae8a809f0e4b7123305ece
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-09-04 00:08:05 +0200

    iter

--- imblearn/tests/test_misc.py
+++ imblearn/tests/test_misc.py
@@ -2,3 +2,9 @@
 
 # Authors: Guillaume Lemaitre <g.lemaitre58@gmail.com>
 # License: MIT
+
+from imblearn.misc import FunctionSampler
+
+
+def function_sampler_identity():
+    sampler = FunctionSampler(1)
