62ac4e47b799534f9954110dbc24534d5b98e9e0
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-10-07 23:12:10 +0200

    MAINT update the version of sphinx-gallery to master

--- build_tools/circle/build_doc.sh
+++ build_tools/circle/build_doc.sh
@@ -93,7 +93,8 @@ source activate $CONDA_ENV_NAME
 
 conda install --yes pip numpy scipy scikit-learn pillow matplotlib sphinx \
       sphinx_rtd_theme numpydoc
-pip install sphinx-gallery==0.1.11
+# install the master version of sphinx-gallery
+pip install -U git+https://github.com/sphinx-gallery/sphinx-gallery.git
 
 # Build and install imbalanced-learn in dev mode
 cd "$HOME/$CIRCLE_PROJECT_REPONAME"
