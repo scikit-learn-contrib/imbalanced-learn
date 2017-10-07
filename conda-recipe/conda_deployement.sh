9842210f32e7d5a95b00bad5e36029e16ee678ca
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-10-07 13:58:22 +0200

    MAINT Update and synchronize python badge in README (#350)

--- conda-recipe/conda_deployement.sh
+++ conda-recipe/conda_deployement.sh
@@ -2,20 +2,16 @@
 
 # This script is used to build and upload package to conda cloud
 # in an automatic manner
-for pyv in {2.7,3.5,3.6} ; do
-    mkdir build_conda
-    conda build --python=$pyv --output-folder build_conda imbalanced-learn
-    conda convert --platform all build_conda/*.tar.bz2 \
-          -o build_conda/
-    dirs=`ls -l "$PWD/build_conda"`
-    for d in build_conda/*/ ; do
-        for file in $d*
-        do
-            if [[ -f $file ]]; then
-                # upload each package
-                anaconda upload $file
-            fi
-        done
+mkdir build_conda
+conda build build_conda imbalanced-learn
+dirs=`ls -l "$PWD/build_conda"`
+for d in build_conda/*/ ; do
+    for file in $d*
+    do
+        if [[ -f $file ]]; then
+            # upload each package
+            anaconda upload $file
+        fi
     done
-    rm -r build_conda
 done
+rm -r build_conda
