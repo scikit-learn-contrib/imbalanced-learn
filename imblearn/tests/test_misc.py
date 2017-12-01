b71dc214ba73236e1aac90ddfeddc7247120ab19
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-12-01 11:21:58 +0100

    TST used sklearn dense sparse test function

--- imblearn/tests/test_misc.py
+++ imblearn/tests/test_misc.py
@@ -9,6 +9,7 @@ from scipy import sparse
 
 from sklearn.datasets import load_iris
 from sklearn.utils.testing import assert_array_equal
+from sklearn.utils.testing import assert_allclose_dense_sparse
 
 from imblearn.datasets import make_imbalance
 from imblearn.misc import FunctionSampler
@@ -25,10 +26,7 @@ X, y = make_imbalance(iris.data, iris.target, ratio={0: 10, 1: 25},
 def test_function_sampler_identity(X, y):
     sampler = FunctionSampler()
     X_res, y_res = sampler.fit_sample(X, y)
-    if sparse.issparse(X):
-        X = X.toarray()
-        X_res = X_res.toarray()
-    assert_array_equal(X_res, X)
+    assert_allclose_dense_sparse(X_res, X)
     assert_array_equal(y_res, y)
 
 
@@ -42,10 +40,7 @@ def test_function_sampler_func(X, y):
 
     sampler = FunctionSampler(func=func)
     X_res, y_res = sampler.fit_sample(X, y)
-    if sparse.issparse(X):
-        X = X.toarray()
-        X_res = X_res.toarray()
-    assert_array_equal(X_res, X[:10])
+    assert_allclose_dense_sparse(X_res, X[:10])
     assert_array_equal(y_res, y[:10])
 
 
@@ -62,9 +57,5 @@ def test_function_sampler_func_kwargs(X, y):
                                                   'random_state': 0})
     X_res, y_res = sampler.fit_sample(X, y)
     X_res_2, y_res_2 = RandomUnderSampler(random_state=0).fit_sample(X, y)
-    if sparse.issparse(X):
-        X = X.toarray()
-        X_res = X_res.toarray()
-        X_res_2 = X_res_2.toarray()
-    assert_array_equal(X_res, X_res_2)
+    assert_allclose_dense_sparse(X_res, X_res_2)
     assert_array_equal(y_res, y_res_2)
