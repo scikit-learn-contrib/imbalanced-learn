6a5ba4521154dc48f0cc7d604a41dcce0add43fd
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-12-01 11:18:50 +0100

    TST add test for sparse matrices

--- imblearn/tests/test_misc.py
+++ imblearn/tests/test_misc.py
@@ -3,6 +3,10 @@
 # Authors: Guillaume Lemaitre <g.lemaitre58@gmail.com>
 # License: MIT
 
+import pytest
+
+from scipy import sparse
+
 from sklearn.datasets import load_iris
 from sklearn.utils.testing import assert_array_equal
 
@@ -15,25 +19,40 @@ X, y = make_imbalance(iris.data, iris.target, ratio={0: 10, 1: 25},
                       random_state=0)
 
 
-def test_function_sampler_identity():
+@pytest.mark.parametrize("X,y", [(X, y),
+                                 (sparse.csr_matrix(X), y),
+                                 (sparse.csc_matrix(X), y)])
+def test_function_sampler_identity(X, y):
     sampler = FunctionSampler()
     X_res, y_res = sampler.fit_sample(X, y)
+    if sparse.issparse(X):
+        X = X.toarray()
+        X_res = X_res.toarray()
     assert_array_equal(X_res, X)
     assert_array_equal(y_res, y)
 
 
-def test_function_sampler_func():
+@pytest.mark.parametrize("X,y", [(X, y),
+                                 (sparse.csr_matrix(X), y),
+                                 (sparse.csc_matrix(X), y)])
+def test_function_sampler_func(X, y):
 
     def func(X, y):
         return X[:10], y[:10]
 
     sampler = FunctionSampler(func=func)
     X_res, y_res = sampler.fit_sample(X, y)
+    if sparse.issparse(X):
+        X = X.toarray()
+        X_res = X_res.toarray()
     assert_array_equal(X_res, X[:10])
     assert_array_equal(y_res, y[:10])
 
 
-def test_function_sampler_func_kwargs():
+@pytest.mark.parametrize("X,y", [(X, y),
+                                 (sparse.csr_matrix(X), y),
+                                 (sparse.csc_matrix(X), y)])
+def test_function_sampler_func_kwargs(X, y):
 
     def func(X, y, ratio, random_state):
         rus = RandomUnderSampler(ratio=ratio, random_state=random_state)
@@ -43,5 +62,9 @@ def test_function_sampler_func_kwargs():
                                                   'random_state': 0})
     X_res, y_res = sampler.fit_sample(X, y)
     X_res_2, y_res_2 = RandomUnderSampler(random_state=0).fit_sample(X, y)
+    if sparse.issparse(X):
+        X = X.toarray()
+        X_res = X_res.toarray()
+        X_res_2 = X_res_2.toarray()
     assert_array_equal(X_res, X_res_2)
     assert_array_equal(y_res, y_res_2)
