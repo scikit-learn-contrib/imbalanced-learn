89e27d9d9eb14b2658e5e377162ca0576c606b96
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-03-29 13:02:04 +0200

    iter

--- imblearn/under_sampling/prototype_generation/cluster_centroids.py
+++ imblearn/under_sampling/prototype_generation/cluster_centroids.py
@@ -84,14 +84,13 @@ ClusterCentroids # doctest: +NORMALIZE_WHITESPACE
     >>> X, y = make_classification(n_classes=2, class_sep=2,
     ... weights=[0.1, 0.9], n_informative=3, n_redundant=1, flip_y=0,
     ... n_features=20, n_clusters_per_class=1, n_samples=1000, random_state=10)
-    >>> print('Original dataset shape %s', Counter(y))
-    ... # doctest: +ELLIPSIS
-    Original dataset shape Counter(...)
+    >>> print('Original dataset shape %s' % Counter(y))
+    Original dataset shape Counter({{1: 900, 0: 100}})
     >>> cc = ClusterCentroids(random_state=42)
     >>> X_res, y_res = cc.fit_sample(X, y)
-    >>> print('Resampled dataset shape %s', Counter(y_res))
+    >>> print('Resampled dataset shape %s' % Counter(y_res))
     ... # doctest: +ELLIPSIS
-    Resampled dataset shape Counter(...)
+    Resampled dataset shape Counter({{...}})
 
     """
 
