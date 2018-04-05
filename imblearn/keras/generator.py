7e138336da3d42b2ea3049a212870beeeb87578d
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-04-06 01:06:28 +0200

    TST test the keras class

--- imblearn/keras/generator.py
+++ imblearn/keras/generator.py
@@ -1,28 +1,90 @@
 """Implement generators for ``keras`` which will balance the data."""
 
-import keras
+try:
+    import keras
+except ImportError:
+    # Skip the tests for the examples
+    import pytest
+    keras = pytest.importorskip('keras')
+    raise ImportError("To use the imblearn.keras module, you need to install "
+                      "keras.")
 
 from sklearn.base import clone
 from sklearn.utils import safe_indexing
+from sklearn.utils import check_random_state
+from sklearn.utils.testing import set_random_state
 
 from ..under_sampling import RandomUnderSampler
 
 
+# FIXME: add docstring for random_state using Substitution
 class BalancedBatchGenerator(keras.utils.Sequence):
-    """
+    """Create balanced batches when training a keras model.
+
+    Create a keras ``Sequence`` which is given to ``fit_generator``. The
+    sampler defines the sampling strategy used to balance the dataset ahead of
+    creating the batch. The sampler should have an attribute
+    ``return_indices``.
+
+    Parameters
+    ----------
+    X : ndarray, shape (n_samples, n_features)
+        Original imbalanced dataset.
+
+    y : ndarray, shape (n_samples,) or (n_samples, n_classes)
+        Associated targets.
+
+    sampler : object or None, optional (default=None)
+        A sampler instance which has an attribute ``return_indices``.
+
+    batch_size : int, optional (default=32)
+        Number of samples per gradient update.
+
+    {random_state}
+
+    Attributes
+    ----------
+    sampler_ : object
+        The sampler used to balance the dataset.
+
+    indices_ : ndarray, shape (n_samples, n_features)
+        The indices of the samples selected during sampling.
+
+    Examples
+    --------
+    >>> from sklearn.datasets import load_iris
+    >>> iris = load_iris()
+    >>> from imblearn.datasets import make_imbalance
+    >>> X, y = make_imbalance(iris.data, iris.target, {0: 30, 1: 50, 2: 40})
+    >>> y = keras.utils.to_categorical(y, 3)
+    >>> import keras
+    >>> model = keras.models.Sequential()
+    >>> model.add(keras.layers.Dense(y.shape[1], input_dim=X.shape[1],
+    ...                              activation='softmax'))
+    >>> model.compile(optimizer='sgd', loss='categorical_crossentropy',
+    ...               metrics=['accuracy'])
+    >>> from imblearn.keras import BalancedBatchGenerator
+    >>> from imblearn.under_sampling import NearMiss
+    >>> training_generator = BalancedBatchGenerator(
+    ...     X, y, sampler=NearMiss(), batch_size=10, random_state=42)
+    >>> callback_history = model.fit_generator(generator=training_generator,
+    ...                                        epochs=10, verbose=0)
+
 
     """
-    def __init__(self, X, y, sampler=None, batch_size=64, stratify=True):
+    def __init__(self, X, y, sampler=None, batch_size=32, random_state=None):
         self.X = X
         self.y = y
         self.sampler = sampler
         self.batch_size = batch_size
-        self.stratify = stratify
+        self.random_state = random_state
         self._sample()
 
     def _sample(self):
+        random_state = check_random_state(self.random_state)
         if self.sampler is None:
-            self.sampler_ = RandomUnderSampler(return_indices=True)
+            self.sampler_ = RandomUnderSampler(return_indices=True,
+                                               random_state=random_state)
         else:
             if not hasattr(self.sampler, 'return_indices'):
                 raise ValueError("'sampler' needs to return the indices of "
@@ -30,8 +92,11 @@ class BalancedBatchGenerator(keras.utils.Sequence):
                                  "which has an attribute 'return_indices'.")
             self.sampler_ = clone(self.sampler)
             self.sampler_.set_params(return_indices=True)
+            set_random_state(self.sampler_, random_state)
 
         _, _, self.indices_ = self.sampler_.fit_sample(self.X, self.y)
+        # shuffle the indices since the sampler are packing them by class
+        random_state.shuffle(self.indices_)
 
     def __len__(self):
         return int(self.indices_.size // self.batch_size)
--- imblearn/keras/tests/test_keras.py
+++ imblearn/keras/tests/test_keras.py
@@ -1,3 +1,7 @@
+import pytest
+
+keras = pytest.importorskip('keras')
+
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.utils import to_categorical
@@ -6,17 +10,42 @@ from sklearn.datasets import load_iris
 
 from imblearn.datasets import make_imbalance
 from imblearn.keras import BalancedBatchGenerator
+from imblearn.under_sampling import ClusterCentroids
+from imblearn.under_sampling import NearMiss
 
 iris = load_iris()
-X, y = iris.data, to_categorical(iris.target, 3)
+X, y = make_imbalance(iris.data, iris.target, {0: 30, 1: 50, 2: 40})
+y = to_categorical(y, 3)
 
 
-def test_balanced_batch_generator():
+def _build_keras_model(n_classes, n_features):
     model = Sequential()
-    model.add(Dense(y.shape[1], input_dim=X.shape[1], activation='softmax'))
+    model.add(Dense(n_classes, input_dim=n_features, activation='softmax'))
     model.compile(optimizer='sgd', loss='categorical_crossentropy',
                   metrics=['accuracy'])
-    training_generator = BalancedBatchGenerator(X, y)
+    return model
+
+
+def test_balanced_batch_generator_class_no_return_indices():
+    model = _build_keras_model(y.shape[1], X.shape[1])
+    with pytest.raises(ValueError, match='needs to return the indices'):
+        training_generator = BalancedBatchGenerator(X, y,
+                                                    sampler=ClusterCentroids(),
+                                                    batch_size=10,
+                                                    random_state=42)
+        model.fit_generator(generator=training_generator,
+                            epochs=10)
+
+
+@pytest.mark.parametrize(
+    "sampler",
+    [None, NearMiss()]
+)
+def test_balanced_batch_generator_class(sampler):
+    model = _build_keras_model(y.shape[1], X.shape[1])
+    training_generator = BalancedBatchGenerator(X, y,
+                                                sampler=sampler,
+                                                batch_size=10,
+                                                random_state=42)
     model.fit_generator(generator=training_generator,
-                        epochs=10,
-                        verbose=10)
+                        epochs=10)
