f08534bb262bc8f3b7681020d9e745cbb675c1e9
EvgeniDubov <32032278+EvgeniDubov@users.noreply.github.com>
2018-07-12 10:37:25 +0300

    Fixed pep8 issues in the setup

--- imblearn/tree_split/setup.py
+++ imblearn/tree_split/setup.py
@@ -1,13 +1,13 @@
-from setuptools   import setup
+from setuptools import setup
 from Cython.Build import cythonize
 import numpy
 
 setup(
-	name='hellinger-distance-criterion',
-	version=0.1,
-	url='github.com/EvgeniDubov/hellinger-random-forest',
+    name='hellinger-distance-criterion',
+    version=0.1,
+    url='github.com/EvgeniDubov/hellinger-distance-criterion',
     author='Evgeni Dubov',
-	author_email='evgeni.dubov@gmail.com',
-	ext_modules = cythonize('hellinger_distance_criterion.pyx'),
-	include_dirs=[numpy.get_include()]
+    author_email='evgeni.dubov@gmail.com',
+    ext_modules=cythonize('hellinger_distance_criterion.pyx'),
+    include_dirs=[numpy.get_include()]
 )
