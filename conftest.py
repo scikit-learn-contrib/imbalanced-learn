7c48491b7ea62f02e5f6bb888fb806e489637e34
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2018-08-22 15:15:42 +0200

    EHN: mini-batches balancing in keras and tensforflow (#409)
    This PR attend to provide some utilities for keras:
    
    - [x] support for one-vs-all encoded targets (#410)
    - [x] balanced batch generator
    
    TODO:
    
    - [x] Add common test to check multiclass == multilabel-indicator (#410)
    - [x] Manage the specificity of the EasyEnsemble and BalanceCascade (overwrite `sample`)
    - [x] Add user guide documentation
    - [x] Add an example for simple use
    - [x] Add an example for deep training
    - [x] Add substitution 
    - [x] What's new
    - [x] Optional depencies

--- conftest.py
+++ conftest.py
@@ -7,8 +7,27 @@
 
 # Set numpy array str/repr to legacy behaviour on numpy > 1.13 to make
 # the doctests pass
+import os
+import pytest
 import numpy as np
+
 try:
     np.set_printoptions(legacy='1.13')
 except TypeError:
     pass
+
+
+def pytest_runtest_setup(item):
+    fname = item.fspath.strpath
+    if (fname.endswith(os.path.join('keras', '_generator.py')) or
+            fname.endswith('miscellaneous.rst')):
+        try:
+            import keras
+        except ImportError:
+            pytest.skip('The keras package is not installed.')
+    elif (fname.endswith(os.path.join('tensorflow', '_generator.py')) or
+          fname.endswith('miscellaneous.rst')):
+        try:
+            import tensorflow
+        except ImportError:
+            pytest.skip('The tensorflow package is not installed.')
