212547c431081c566ba3892c4af68fe1c0eeed62
chkoar <ichkoar@gmail.com>
2018-08-25 17:19:24 +0300

    add memory to make_pipeline function

--- imblearn/pipeline.py
+++ imblearn/pipeline.py
@@ -600,15 +600,44 @@ def _fit_sample_one(sampler, X, y, **fit_params):
     return X_res, y_res, sampler
 
 
-def make_pipeline(*steps):
+def make_pipeline(*steps, **kwargs):
     """Construct a Pipeline from the given estimators.
-
     This is a shorthand for the Pipeline constructor; it does not require, and
     does not permit, naming the estimators. Instead, their names will be set
     to the lowercase of their types automatically.
-
+    Parameters
+    ----------
+    *steps : list of estimators.
+    memory : None, str or object with the joblib.Memory interface, optional
+        Used to cache the fitted transformers of the pipeline. By default,
+        no caching is performed. If a string is given, it is the path to
+        the caching directory. Enabling caching triggers a clone of
+        the transformers before fitting. Therefore, the transformer
+        instance given to the pipeline cannot be inspected
+        directly. Use the attribute ``named_steps`` or ``steps`` to
+        inspect estimators within the pipeline. Caching the
+        transformers is advantageous when fitting is time consuming.
+    See also
+    --------
+    imblearn.pipeline.Pipeline : Class for creating a pipeline of
+        transforms with a final estimator.
+    Examples
+    --------
+    >>> from sklearn.naive_bayes import GaussianNB
+    >>> from sklearn.preprocessing import StandardScaler
+    >>> make_pipeline(StandardScaler(), GaussianNB(priors=None))
+    ...     # doctest: +NORMALIZE_WHITESPACE
+    Pipeline(memory=None,
+             steps=[('standardscaler',
+                     StandardScaler(copy=True, with_mean=True, with_std=True)),
+                    ('gaussiannb',
+                     GaussianNB(priors=None, var_smoothing=1e-09))])
     Returns
     -------
     p : Pipeline
     """
-    return Pipeline(pipeline._name_estimators(steps))
+    memory = kwargs.pop('memory', None)
+    if kwargs:
+        raise TypeError('Unknown keyword arguments: "{}"'
+                        .format(list(kwargs.keys())[0]))
+    return Pipeline(pipeline._name_estimators(steps), memory=memory)
