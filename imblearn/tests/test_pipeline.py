c32fc94341d765bd4297686eb16fd6048731983d
Oliver Rausch <oliverrausch99@gmail.com>
2019-02-27 10:15:37 +0100

    Add failing test

--- imblearn/tests/test_pipeline.py
+++ imblearn/tests/test_pipeline.py
@@ -1104,3 +1104,21 @@ def test_predict_with_predict_params():
     pipe.fit(None, None)
     pipe.predict(X=None, got_attribute=True)
     assert pipe.named_steps['clf'].got_attribute
+
+def test_resampler_last_stage_passthrough():
+
+    X, y = make_classification(
+        n_classes=2,
+        class_sep=2,
+        weights=[0.1, 0.9],
+        n_informative=3,
+        n_redundant=1,
+        flip_y=0,
+        n_features=20,
+        n_clusters_per_class=1,
+        n_samples=50000,
+        random_state=0)
+
+    rus = RandomUnderSampler(random_state=42)
+    pipe = make_pipeline(rus, None)
+    pipe.fit_resample(X, y)
