c2fc4dac25563ec2424a1ccabdb624b91a6b415f
Thomas Kluiters <thomas.kluiters@gmail.com>
2019-05-05 21:23:01 +0200

    Fix doctest error

--- imblearn/over_sampling/_smote.py
+++ imblearn/over_sampling/_smote.py
@@ -1199,7 +1199,7 @@ class SMOTEN(SMOTE):
     ... weights=[0.1, 0.9], n_informative=3, n_redundant=1, flip_y=0,
     ... n_features=5, n_clusters_per_class=1, n_samples=1000, random_state=10)
     >>> print('Original dataset shape (%s, %s)' % X.shape)
-    Original dataset shape (1000, 20)
+    Original dataset shape (1000, 5)
     >>> print('Original dataset samples per class {}'.format(Counter(y)))
     Original dataset samples per class Counter({1: 900, 0: 100})
     >>> # simulate the 2 last columns to be categorical features
