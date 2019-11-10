afbf78172cc817274bb13fba90ab4857f7ae5782
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2019-11-10 15:27:42 +0100

    MNT install latex packages for math equation in doc

--- build_tools/circle/build_doc.sh
+++ build_tools/circle/build_doc.sh
@@ -74,6 +74,15 @@ fi
 
 MAKE_TARGET=html
 
+# Installing required system packages to support the rendering of math
+# notation in the HTML documentation
+sudo -E apt-get -yq update
+sudo -E apt-get -yq remove texlive-binaries --purge
+sudo -E apt-get -yq --no-install-suggests --no-install-recommends \
+    install dvipng texlive-latex-base texlive-latex-extra \
+    texlive-latex-recommended texlive-fonts-recommended \
+    latexmk gsfonts ccache
+
 # deactivate circleci virtualenv and setup a miniconda env instead
 if [[ `type -t deactivate` ]]; then
     deactivate
