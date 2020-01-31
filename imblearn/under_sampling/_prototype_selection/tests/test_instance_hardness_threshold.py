79699733bbd1a5460278c34dfd266101d9c5b087
chkoar <ichkoar@gmail.com>
2020-01-31 14:55:41 +0200

    Update test_iht_fit_resample_half

--- imblearn/under_sampling/_prototype_selection/tests/test_instance_hardness_threshold.py
+++ imblearn/under_sampling/_prototype_selection/tests/test_instance_hardness_threshold.py
@@ -55,13 +55,13 @@ def test_iht_fit_resample():
 
 
 def test_iht_fit_resample_half():
-    sampling_strategy = {0: 6, 1: 8}
+    sampling_strategy = {0: 3, 1: 3}
     iht = InstanceHardnessThreshold(
         ESTIMATOR, sampling_strategy=sampling_strategy, random_state=RND_SEED
     )
     X_resampled, y_resampled = iht.fit_resample(X, Y)
-    assert X_resampled.shape == (14, 2)
-    assert y_resampled.shape == (14,)
+    assert X_resampled.shape == (3, 2)
+    assert y_resampled.shape == (3,)
 
 
 def test_iht_fit_resample_class_obj():
