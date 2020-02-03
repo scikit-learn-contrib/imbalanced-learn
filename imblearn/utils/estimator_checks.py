182dc6e0266cc966fbbe73847636a49f20dde19f
chkoar <ichkoar@gmail.com>
2020-02-03 14:17:54 +0200

    Add missing estimator check

--- imblearn/utils/estimator_checks.py
+++ imblearn/utils/estimator_checks.py
@@ -41,6 +41,7 @@ def _yield_sampler_checks(name, Estimator):
     yield check_samplers_sampling_strategy_fit_resample
     yield check_samplers_sparse
     yield check_samplers_pandas
+    yield check_samplers_list
     yield check_samplers_multiclass_ova
     yield check_samplers_preserve_dtype
     yield check_samplers_sample_indices
