c150d59d300cd53db45fafdf667649d99ab79dbc
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2020-02-16 11:47:19 +0100

    get nightly build from anaconda

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -63,7 +63,7 @@ elif [[ "$DISTRIB" == "ubuntu" ]]; then
     virtualenv --system-site-packages --python=python3 testvenv
     source testvenv/bin/activate
 
-    pip3 install --pre -f https://sklearn-nightly.scdn8.secure.raxcdn.com scikit-learn
+    pip3 install --pre --extra-index https://pypi.anaconda.org/scipy-wheels-nightly/simple scikit-learn
     pip3 install pandas
     pip3 install pytest pytest-cov codecov sphinx numpydoc
 
