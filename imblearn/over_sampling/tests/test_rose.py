53c7a8d1f9f9aa6828f5a14f87bddeeebfbbe1ef
Andrea Lorenzon <andrea.lorenzon@ceric-eric.eu>
2020-08-21 11:34:32 +0200

    Created empty test units

---
+++ imblearn/over_sampling/tests/test_rose.py
@@ -0,0 +1,14 @@
+"""Test the module SMOTENC."""
+# Authors: Andrea Lorenzon <andrelorenzon@gmail.com>
+# License: MIT
+
+import pytest
+
+import numpy as np
+
+def test_testunit():
+    return True
+
+def test_randomState():
+    assert(np.random.RandomState(42))
+
