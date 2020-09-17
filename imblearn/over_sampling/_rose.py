94ecd4d4574cedcbac2b170c4b22cd160fef15a7
Andrea Lorenzon <andrea.lorenzon@ceric-eric.eu>
2020-09-17 11:53:31 +0200

    pep8 fix

--- imblearn/over_sampling/_rose.py
+++ imblearn/over_sampling/_rose.py
@@ -109,8 +109,7 @@ class ROSE(BaseOverSampler):
 
         if self.shrink_factors is None:
             self.shrink_factors = {
-                key: 1 for key in self.sampling_strategy_.keys()
-                }
+                key: 1 for key in self.sampling_strategy_.keys()}
 
         for class_sample, n_samples in self.sampling_strategy_.items():
             class_indices = np.flatnonzero(y == class_sample)
