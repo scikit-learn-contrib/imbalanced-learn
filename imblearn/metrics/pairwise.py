efaa4b09575bcc70ff27c38a792dbc9cc440880e
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2021-02-13 21:55:47 +0100

    FEA add ValueDifferenceMetric to compute distance between nominal samples

---
+++ imblearn/metrics/pairwise.py
@@ -0,0 +1,138 @@
+import numpy as np
+from scipy.spatial import distance_matrix
+
+
+class ValueDifferenceMetric:
+    """Class implementing the Value Difference Metric.
+
+    This metric computes the distance between samples containing only nominal
+    categorical features. The distance between feature values of two samples
+    is defined as:
+
+    .. math::
+       \delta(x, y) = \sum_{c=1}^{C} |p(c|x_{f}) - p(c|y_{f})|^{k} \ ,
+
+    where :math:`x` and :math:`y` are two samples and :math:`f` a given
+    feature, :math:`C` is the number of classes, :math:`p(c|x_{f})` is the
+    conditional probability that the output class is :math:`c` given that
+    the feature value :math:`f` has the value :math:`x` and :math:`k` an
+    exponent usually defined to 1 or 2.
+
+    The distance for the feature vectors :math:`X` and :math:`Y` is
+    subsequently defined as:
+
+    .. math::
+       \Delta(X, Y) = \sum_{f=1}^{F} \delta(X_{f}, Y_{f})^{r} \ ,
+
+    where :math:`F` is the number of feature and :math:`r` an exponent usually
+    defined equal to 1 or 2.
+
+    Parameters
+    ----------
+    classes : ndarray of shape (n_classes,)
+        The unique labels in `y`.
+
+    categories : list of arrays
+        List of arrays containing the categories for each feature. You can pass
+        the fitted attribute `categories_` of the
+        :class:`~sklearn.preprocesssing.OrdinalEncoder` used to encode the
+        data.
+
+    k : int, default=1
+        Exponent used to compute the distance between feature value.
+
+    r : int, default=2
+        Exponent used to compute the distance between the feature vector.
+
+    Attributes
+    ----------
+    proba_per_class_ : list of ndarray of shape (n_categories, n_classes)
+        List of length `n_features` containing the conditional probabilities
+        for each category given a class.
+    """
+
+    def __init__(self, classes, categories, k=1, r=2):
+        self.classes = classes
+        self.categories = categories
+        self.k = k
+        self.r = r
+
+    def fit(self, X, y):
+        """Compute the necessary statistics from the training set.
+
+        Parameters
+        ----------
+        X : ndarray of shape (n_samples, n_features)
+            The input data. The data are expected to be encoded with an
+            :class:`~sklearn.preprocessing.OrdinalEncoder`.
+
+        y : ndarray of shape (n_features,)
+            The target.
+
+        Returns
+        -------
+        self
+        """
+        n_features = X.shape[1]
+
+        # list of length n_features of ndarray (n_categories, n_classes)
+        counts_per_class = [
+            np.transpose(
+                [
+                    np.bincount(
+                        X[y == klass, feature_idx],
+                        minlength=len(self.categories[feature_idx]),
+                    )
+                    for klass in self.classes
+                ]
+            )
+            for feature_idx in range(n_features)
+        ]
+
+        # list of length n_features of ndarray (n_categories,)
+        proba_per_class = [
+            (
+                counts_per_class[feature_idx]
+                / counts_per_class[feature_idx].sum(axis=1)[:, np.newaxis]
+            )
+            for feature_idx in range(n_features)
+        ]
+
+        self.proba_per_class_ = proba_per_class
+
+        return self
+
+    def pairwise(self, X1, X2=None):
+        """Compute the VDM distance pairwise.
+
+        Parameters
+        ----------
+        X1 : ndarray of shape (n_samples, n_features)
+            The input data. The data are expected to be encoded with an
+            :class:`~sklearn.preprocessing.OrdinalEncoder`.
+
+        X2 : ndarray of shape (n_samples, n_features)
+            The input data. The data are expected to be encoded with an
+            :class:`~sklearn.preprocessing.OrdinalEncoder`.
+
+        Returns
+        -------
+        distance_matrix : ndarray of shape (n_samples, n_samples)
+            The VDM pairwise distance.
+        """
+        n_samples_X1, n_features = X1.shape
+        n_samples_X2 = X2.shape[0] if X2 is not None else n_samples_X1
+
+        distance = np.zeros(shape=(n_samples_X1, n_samples_X2), dtype=np.float64)
+        for feature_idx in range(n_features):
+            proba_feature_X1 = self.proba_per_class_[feature_idx][X1[:, feature_idx]]
+            if X2 is not None:
+                proba_feature_X2 = self.proba_per_class_[feature_idx][
+                    X2[:, feature_idx]
+                ]
+            else:
+                proba_feature_X2 = proba_feature_X1
+            distance += (
+                distance_matrix(proba_feature_X1, proba_feature_X2, p=self.k) ** self.r
+            )
+        return distance
