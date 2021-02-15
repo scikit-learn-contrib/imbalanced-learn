57f839f07d12cfae541c2a8cf34bd7bf24bbd1e4
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2021-02-15 23:53:02 +0100

    DOC add whats new

--- imblearn/over_sampling/tests/test_smoten.py
+++ imblearn/over_sampling/tests/test_smoten.py
@@ -1,5 +1,3 @@
-from collections import Counter
-
 import numpy as np
 import pytest
 
@@ -22,9 +20,35 @@ def data():
 
 
 def test_smoten(data):
+    # overall check for SMOTEN
     X, y = data
-    print(X, y)
     sampler = SMOTEN(random_state=0)
     X_res, y_res = sampler.fit_resample(X, y)
-    print(X_res, y_res)
-    print(Counter(y_res))
+
+    assert X_res.shape == (80, 3)
+    assert y_res.shape == (80,)
+
+
+def test_smoten_resampling():
+    # check if the SMOTEN resample data as expected
+    # we generate data such that "not apple" will be the minority class and
+    # samples from this class will be generated. We will force the "blue"
+    # category to be associated with this class. Therefore, the new generated
+    # samples should as well be from the "blue" category.
+    X = np.array(["green"] * 5 + ["red"] * 10 + ["blue"] * 7, dtype=object).reshape(
+        -1, 1
+    )
+    y = np.array(
+        ["apple"] * 5
+        + ["not apple"] * 3
+        + ["apple"] * 7
+        + ["not apple"] * 5
+        + ["apple"] * 2,
+        dtype=object,
+    )
+    sampler = SMOTEN(random_state=0)
+    X_res, y_res = sampler.fit_resample(X, y)
+
+    X_generated, y_generated = X_res[X.shape[0] :], y_res[X.shape[0] :]
+    np.testing.assert_array_equal(X_generated, "blue")
+    np.testing.assert_array_equal(y_generated, "not apple")
