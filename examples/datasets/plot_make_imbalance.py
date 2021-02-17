d1409c168b7409ae08afc1434143e18b66d6296c
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2021-02-17 12:21:53 +0100

    iter

--- examples/datasets/plot_make_imbalance.py
+++ examples/datasets/plot_make_imbalance.py
@@ -6,7 +6,6 @@ Create an imbalanced dataset
 An illustration of the :func:`~imblearn.datasets.make_imbalance` function to
 create an imbalanced dataset from a balanced dataset. We show the ability of
 :func:`~imblearn.datasets.make_imbalance` of dealing with Pandas DataFrame.
-
 """
 
 # Authors: Dayvid Oliveira
@@ -14,35 +13,46 @@ create an imbalanced dataset from a balanced dataset. We show the ability of
 #          Guillaume Lemaitre <g.lemaitre58@gmail.com>
 # License: MIT
 
-from collections import Counter
-
-import pandas as pd
-import matplotlib.pyplot as plt
-
-from sklearn.datasets import make_moons
-
-from imblearn.datasets import make_imbalance
-
+# %%
 print(__doc__)
 
+import seaborn as sns
+
+sns.set_context("poster")
+
+# %% [markdown]
 # Generate the dataset
+# --------------------
+#
+# First, we will generate a dataset and convert it to a
+# :class:`~pandas.DataFrame` with arbitrary column names. We will plot the
+# original dataset.
+
+# %%
+import pandas as pd
+from sklearn.datasets import make_moons
+
 X, y = make_moons(n_samples=200, shuffle=True, noise=0.5, random_state=10)
 X = pd.DataFrame(X, columns=["feature 1", "feature 2"])
-
-# Two subplots, unpack the axes array immediately
-f, axs = plt.subplots(2, 3)
-
-axs = [a for ax in axs for a in ax]
-
-X.plot.scatter(
+ax = X.plot.scatter(
     x="feature 1",
     y="feature 2",
     c=y,
-    ax=axs[0],
     colormap="viridis",
     colorbar=False,
 )
-axs[0].set_title("Original set")
+sns.despine(ax=ax, offset=10)
+
+# %% [markdown]
+# Make a dataset imbalanced
+# -------------------------
+#
+# Now, we will show the helpers :func:`~imblearn.datasets.make_imbalance`
+# that is useful to random select a subset of samples. It will impact the
+# class distribution as specified by the parameters.
+
+# %%
+from collections import Counter
 
 
 def ratio_func(y, multiplier, minority_class):
@@ -50,25 +60,41 @@ def ratio_func(y, multiplier, minority_class):
     return {minority_class: int(multiplier * target_stats[minority_class])}
 
 
-multipliers = [0.9, 0.75, 0.5, 0.25, 0.1]
-for i, multiplier in enumerate(multipliers, start=1):
-    ax = axs[i]
+# %%
+import matplotlib.pyplot as plt
+from imblearn.datasets import make_imbalance
 
-    X_, y_ = make_imbalance(
+fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(15, 10))
+
+X.plot.scatter(
+    x="feature 1",
+    y="feature 2",
+    c=y,
+    ax=axs[0, 0],
+    colormap="viridis",
+    colorbar=False,
+)
+axs[0, 0].set_title("Original set")
+sns.despine(ax=axs[0, 0], offset=10)
+
+multipliers = [0.9, 0.75, 0.5, 0.25, 0.1]
+for ax, multiplier in zip(axs.ravel()[1:], multipliers):
+    X_resampled, y_resampled = make_imbalance(
         X,
         y,
         sampling_strategy=ratio_func,
         **{"multiplier": multiplier, "minority_class": 1},
     )
-    X_.plot.scatter(
+    X_resampled.plot.scatter(
         x="feature 1",
         y="feature 2",
-        c=y_,
+        c=y_resampled,
         ax=ax,
         colormap="viridis",
         colorbar=False,
     )
     ax.set_title(f"Sampling ratio = {multiplier}")
+    sns.despine(ax=ax, offset=10)
 
 plt.tight_layout()
 plt.show()
