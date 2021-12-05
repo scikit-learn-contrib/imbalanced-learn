c7b478844a169e6691458f7642b6470e73afc304
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2021-12-05 16:00:06 +0100

    iter

--- build_tools/azure/install.sh
+++ build_tools/azure/install.sh
@@ -7,106 +7,114 @@ UNAMESTR=`uname`
 
 make_conda() {
     TO_INSTALL="$@"
-    conda create -n $VIRTUALENV --yes $TO_INSTALL -c conda-forge
+    if [[ "$DISTRIB" == *"mamba"* ]]; then
+        mamba create -n $VIRTUALENV --yes $TO_INSTALL
+    else
+        conda config --show
+        conda create -n $VIRTUALENV --yes $TO_INSTALL
+    fi
     source activate $VIRTUALENV
 }
 
-version_ge() {
-    # The two version numbers are seperated with a new line is piped to sort
-    # -rV. The -V activates for version number sorting and -r sorts in
-    # decending order. If the first argument is the top element of the sort, it
-    # is greater than or equal to the second argument.
-    test "$(printf "${1}\n${2}" | sort -rV | head -n 1)" == "$1"
+setup_ccache() {
+    echo "Setting up ccache"
+    mkdir /tmp/ccache/
+    which ccache
+    for name in gcc g++ cc c++ x86_64-linux-gnu-gcc x86_64-linux-gnu-c++; do
+      ln -s $(which ccache) "/tmp/ccache/${name}"
+    done
+    export PATH="/tmp/ccache/:${PATH}"
+    ccache -M 256M
 }
 
-if [[ "$DISTRIB" == "conda" ]]; then
+# imports get_dep
+source build_tools/shared.sh
 
-    TO_INSTALL="python=$PYTHON_VERSION pip \
-                numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION \
-                joblib=$JOBLIB_VERSION git"
+if [[ "$DISTRIB" == "conda" || "$DISTRIB" == *"mamba"* ]]; then
 
-    if [[ "$INSTALL_MKL" == "true" ]]; then
-        TO_INSTALL="$TO_INSTALL mkl"
+    if [[ "$CONDA_CHANNEL" != "" ]]; then
+        TO_INSTALL="--override-channels -c $CONDA_CHANNEL"
     else
-        TO_INSTALL="$TO_INSTALL nomkl"
+        TO_INSTALL=""
     fi
 
-    make_conda $TO_INSTALL
+    TO_INSTALL="$TO_INSTALL python=$PYTHON_VERSION"
+    TO_INSTALL="$TO_INSTALL ccache pip blas[build=$BLAS]"
 
-    TO_INSTALL=""
+    TO_INSTALL="$TO_INSTALL $(get_dep numpy $NUMPY_VERSION)"
+    TO_INSTALL="$TO_INSTALL $(get_dep scipy $SCIPY_VERSION)"
+    TO_INSTALL="$TO_INSTALL $(get_dep cython $CYTHON_VERSION)"
+    TO_INSTALL="$TO_INSTALL $(get_dep joblib $JOBLIB_VERSION)"
+    TO_INSTALL="$TO_INSTALL $(get_dep pandas $PANDAS_VERSION)"
+    TO_INSTALL="$TO_INSTALL $(get_dep Pillow $PILLOW_VERSION)"
+    TO_INSTALL="$TO_INSTALL $(get_dep matplotlib $MATPLOTLIB_VERSION)"
 
-    if [[ -n "$SKLEARN_VERSION" ]]; then
-        TO_INSTALL="$TO_INSTALL scikit-learn=$SKLEARN_VERSION"
-    else
-        TO_INSTALL="$TO_INSTALL scikit-learn"
-    fi
-
-    if [[ -n "$PANDAS_VERSION" ]]; then
-        TO_INSTALL="$TO_INSTALL pandas=$PANDAS_VERSION"
-    fi
-
-    if [[ -n "$KERAS_VERSION" ]]; then
-        TO_INSTALL="$TO_INSTALL keras=$KERAS_VERSION tensorflow=1"
-        KERAS_BACKEND=tensorflow
-    fi
-
-    if [[ -n "$TENSORFLOW_VERSION" ]]; then
-        TO_INSTALL="$TO_INSTALL tensorflow=$TENSORFLOW_VERSION"
-    fi
-
-    if [[ "$PYTEST_VERSION" == "*" ]]; then
-        python -m pip install pytest
-    else
-        python -m pip install pytest=="$PYTEST_VERSION"
-    fi
-
-    if [[ "$PYTHON_VERSION" == "*" ]]; then
-        python -m pip install pytest-xdist
-    fi
-
-    if [[ -n "$TO_INSTALL" ]]; then
-       conda install --yes $TO_INSTALL -c conda-forge
-    fi
-
-    if [[ -n "$KERAS_VERSION" ]]; then
-        python -c "import keras.backend"
-        sed -i -e 's/"backend":[[:space:]]*"[^"]*/"backend":\ "'$KERAS_BACKEND'/g' ~/.keras/keras.json;
-    fi
+	make_conda $TO_INSTALL
+    setup_ccache
 
 elif [[ "$DISTRIB" == "ubuntu" ]]; then
     sudo add-apt-repository --remove ppa:ubuntu-toolchain-r/test
     sudo apt-get update
-    sudo apt-get install python3-scipy libatlas3-base libatlas-base-dev python3-virtualenv git
+    sudo apt-get install python3-scipy python3-sklearn python3-matplotlib \
+        libatlas3-base libatlas-base-dev python3-virtualenv ccache
     python3 -m virtualenv --system-site-packages --python=python3 $VIRTUALENV
     source $VIRTUALENV/bin/activate
-    python -m pip install pytest==$PYTEST_VERSION pytest-cov joblib cython
-    python -m pip install scikit-learn
-elif [[ "$DISTRIB" == "ubuntu-32" ]]; then
+    setup_ccache
+    python -m pip install $(get_dep cython $CYTHON_VERSION) \
+                          $(get_dep joblib $JOBLIB_VERSION)
+
+elif [[ "$DISTRIB" == "debian-32" ]]; then
     apt-get update
-    apt-get install -y python3-dev python3-scipy libatlas3-base libatlas-base-dev python3-virtualenv git
+    apt-get install -y python3-dev python3-numpy python3-scipy python3-sklearn \
+        python3-matplotlib libatlas3-base libatlas-base-dev python3-virtualenv \
+        python3-pandas ccache
+
     python3 -m virtualenv --system-site-packages --python=python3 $VIRTUALENV
     source $VIRTUALENV/bin/activate
-    python -m pip install pytest==$PYTEST_VERSION pytest-cov joblib cython
-    python -m pip install scikit-learn
+    setup_ccache
+    python -m pip install $(get_dep cython $CYTHON_VERSION) \
+                          $(get_dep joblib $JOBLIB_VERSION)
+
 elif [[ "$DISTRIB" == "conda-pip-latest" ]]; then
     # Since conda main channel usually lacks behind on the latest releases,
     # we use pypi to test against the latest releases of the dependencies.
     # conda is still used as a convenient way to install Python and pip.
-    make_conda "python=$PYTHON_VERSION"
+    make_conda "ccache python=$PYTHON_VERSION"
+    setup_ccache
     python -m pip install -U pip
-    python -m pip install numpy scipy joblib cython
-    python -m pip install scikit-learn
-    python -m pip install pytest==$PYTEST_VERSION pytest-cov pytest-xdist
-    python -m pip install pandas
+
+    python -m pip install pandas matplotlib
+
+elif [[ "$DISTRIB" == "conda-pip-scipy-dev" ]]; then
+    make_conda "ccache python=$PYTHON_VERSION"
+    python -m pip install -U pip
+    echo "Installing numpy and scipy master wheels"
+    dev_anaconda_url=https://pypi.anaconda.org/scipy-wheels-nightly/simple
+    pip install --pre --upgrade --timeout=60 --extra-index $dev_anaconda_url numpy pandas scipy scikit-learn
+    pip install --pre cython
+    setup_ccache
+    echo "Installing joblib master"
+    pip install https://github.com/joblib/joblib/archive/master.zip
+    echo "Installing pillow master"
+    pip install https://github.com/python-pillow/Pillow/archive/main.zip
 fi
 
+python -m pip install $(get_dep threadpoolctl $THREADPOOLCTL_VERSION) \
+                      $(get_dep pytest $PYTEST_VERSION) \
+                      $(get_dep pytest-xdist $PYTEST_XDIST_VERSION)
+
 if [[ "$COVERAGE" == "true" ]]; then
-    python -m pip install coverage codecov pytest-cov
+    python -m pip install codecov pytest-cov
+fi
+
+if [[ "$PYTEST_XDIST_VERSION" != "none" ]]; then
+    python -m pip install pytest-xdist
 fi
 
 if [[ "$TEST_DOCSTRINGS" == "true" ]]; then
+    # numpydoc requires sphinx
     python -m pip install sphinx
-    python -m pip install -U git+https://github.com/numpy/numpydoc.git
+    python -m pip install numpydoc
 fi
 
 python --version
@@ -119,8 +127,12 @@ try:
 except ImportError:
     print('pandas not installed')
 "
-python -m pip list
 
-# Use setup.py instead of `pip install -e .` to be able to pass the -j flag
-# to speed-up the building multicore CI machines.
-python setup.py develop
+python -m pip list
+if [[ "$DISTRIB" == "conda-pip-latest" ]]; then
+    # Check that pip can automatically build scikit-learn with the build
+    # dependencies specified in pyproject.toml using an isolated build
+    # environment:
+    pip install --verbose --editable .
+fi
+ccache -s
