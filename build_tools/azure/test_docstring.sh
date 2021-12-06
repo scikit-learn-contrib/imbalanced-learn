992f8b7ff4845fe8447b108a421f7127f04909ac
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2021-12-06 10:19:49 +0100

    iter

--- build_tools/azure/test_docstring.sh
+++ build_tools/azure/test_docstring.sh
@@ -2,6 +2,10 @@
 
 set -e
 
-if [[ "$TEST_DOCSTRINGS" == 'true' ]]; then
-    pytest -vsl maint_tools/test_docstring.py
+if [[ "$DISTRIB" =~ ^conda.* ]]; then
+    source activate $VIRTUALENV
+elif [[ "$DISTRIB" == "ubuntu" ]]; then
+    source $VIRTUALENV/bin/activate
 fi
+
+pytest -vsl maint_tools/test_docstring.py
