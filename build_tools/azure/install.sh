0aa328ecedfc5741d70b51144e162c386611e273
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2021-12-08 00:46:09 +0100

    iter

--- build_tools/azure/install.sh
+++ build_tools/azure/install.sh
@@ -9,7 +9,7 @@ make_conda() {
     conda update -yq conda
     TO_INSTALL="$@"
     if [[ "$DISTRIB" == *"mamba"* ]]; then
-        conda install mamba
+        conda install mamba -c conda-forge
         mamba create -n $VIRTUALENV --yes $TO_INSTALL
     else
         conda config --show
