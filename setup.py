b3a3dce17491b2251280f6e445fa7f22ffcedca3
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2022-01-11 21:45:50 +0100

    Add support Python 3.10

--- setup.py
+++ setup.py
@@ -49,10 +49,10 @@ CLASSIFIERS = [
     "Operating System :: POSIX",
     "Operating System :: Unix",
     "Operating System :: MacOS",
-    "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
 ]
 INSTALL_REQUIRES = (min_deps.tag_to_packages["install"],)
 EXTRAS_REQUIRE = {
