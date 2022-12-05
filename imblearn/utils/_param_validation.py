347209d9b5cb298a30fb246a8aec4baaef407fb3
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2022-12-05 11:10:26 +0100

    MAINT add support for dataframe

--- imblearn/utils/_param_validation.py
+++ imblearn/utils/_param_validation.py
@@ -20,7 +20,10 @@ from ..utils.fixes import _is_arraylike_not_scalar
 
 sklearn_version = parse_version(sklearn.__version__)
 
-if sklearn_version < parse_version("1.2"):
+# if sklearn_version < parse_version("1.2"):
+if True:
+    # TODO: remove `if True` when we have clear support for:
+    # - dataframe
 
     def validate_parameter_constraints(parameter_constraints, params, caller_name):
         """Validate types and values of given parameters.
@@ -35,6 +38,7 @@ if sklearn_version < parse_version("1.2"):
             Constraints can be:
             - an Interval object, representing a continuous or discrete range of numbers
             - the string "array-like"
+            - the string "dataframe"
             - the string "sparse matrix"
             - the string "random_state"
             - callable
@@ -115,6 +119,8 @@ if sklearn_version < parse_version("1.2"):
             return _ArrayLikes()
         if isinstance(constraint, str) and constraint == "sparse matrix":
             return _SparseMatrices()
+        if isinstance(constraint, str) and constraint == "dataframe":
+            return _DataFrames()
         if isinstance(constraint, str) and constraint == "random_state":
             return _RandomStates()
         if constraint is callable:
@@ -465,6 +471,15 @@ if sklearn_version < parse_version("1.2"):
 
         def __str__(self):
             return "a sparse matrix"
+
+    class _DataFrames(_Constraint):
+        """Constraint representing a DataFrame"""
+
+        def is_satisfied_by(self, val):
+            return hasattr(val, "__dataframe__")
+
+        def __str__(self):
+            return "a DataFrame"
 
     class _Callables(_Constraint):
         """Constraint representing callables."""
@@ -844,6 +859,11 @@ if sklearn_version < parse_version("1.2"):
 
         if isinstance(constraint, _SparseMatrices):
             return csr_matrix([[0, 1], [1, 0]])
+
+        if isinstance(constraint, _DataFrames):
+            import pandas as pd
+
+            return pd.DataFrame({"a": [1, 2, 3]})
 
         if isinstance(constraint, _RandomStates):
             return np.random.RandomState(42)
