a6a7848908e3ac97610dd709e1bb22500fcd7763
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2023-07-07 15:11:57 +0200

    iter

--- build_tools/azure/install.sh
+++ build_tools/azure/install.sh
@@ -41,7 +41,7 @@ if [[ "$DISTRIB" == "conda" || "$DISTRIB" == *"mamba"* ]]; then
 
 	make_conda $TO_INSTALL
 
-    if [[ "$CONDA_CHANNEL" == "" ] && [ "$DISTRIB" == "conda" ]]; then
+    if [[ "$CONDA_CHANNEL" == "" && "$DISTRIB" == "conda" ]]; then
         mamba install scikit-learn==1.3 -c conda-forge --yes
     fi
 
