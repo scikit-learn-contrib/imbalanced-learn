8d4508f7a65340db4b70692d013a778d4339c216
Soledad Galli <solegalli@protonmail.com>
2023-07-11 13:37:58 +0200

    reword
    Co-authored-by: Guillaume Lemaitre <g.lemaitre58@gmail.com>

--- imblearn/under_sampling/_prototype_selection/_neighbourhood_cleaning_rule.py
+++ imblearn/under_sampling/_prototype_selection/_neighbourhood_cleaning_rule.py
@@ -72,7 +72,7 @@ class NeighbourhoodCleaningRule(BaseCleaningSampler):
 
         Ci > C x T ,
 
-        where Ci is the number of samples in the class, C is the number of samples in
+        where Ci is the number of samples in the class to be under-sampled, C is the number of samples in
         the data set, and T is the threshold.
 
     {n_jobs}
