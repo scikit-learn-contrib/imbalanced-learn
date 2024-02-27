065f478face303c0e8aedf52f565dd580b79f0be
Nilesh Patra <nilesh@debian.org>
2024-02-27 15:36:05 +0100

    Distutils is deprecated, drop it
    This patch was needed in Debian
    
    Co-authored-by: Michael R. Crusoe <crusoe@debian.org>

--- imblearn/tensorflow/tests/test_generator.py
+++ imblearn/tensorflow/tests/test_generator.py
@@ -1,4 +1,4 @@
-from distutils.version import LooseVersion
+from packaging.version import parse
 
 import numpy as np
 import pytest
@@ -147,7 +147,7 @@ def check_balanced_batch_generator_tf_2_X_X_compat_1_X_X(dataset, sampler):
 
 @pytest.mark.parametrize("sampler", [None, NearMiss(), RandomOverSampler()])
 def test_balanced_batch_generator(data, sampler):
-    if LooseVersion(tf.__version__) < "2":
+    if parse(tf.__version__) < "2":
         check_balanced_batch_generator_tf_1_X_X(data, sampler)
     else:
         check_balanced_batch_generator_tf_2_X_X_compat_1_X_X(data, sampler)
