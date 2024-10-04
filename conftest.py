980cc944a5dbfc4b6dc3ecff657b8fe155f66e2b
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2024-10-04 18:21:19 +0200

    MAINT compatibility scikit-learn 1.5.2 and Numpy 2 (#1097)

--- conftest.py
+++ conftest.py
@@ -7,7 +7,14 @@
 
 import os
 
+import numpy as np
 import pytest
+from sklearn.utils.fixes import parse_version
+
+# use legacy numpy print options to avoid failures due to NumPy 2.+ scalar
+# representation
+if parse_version(np.__version__) > parse_version("2.0.0"):
+    np.set_printoptions(legacy="1.25")
 
 
 def pytest_runtest_setup(item):
